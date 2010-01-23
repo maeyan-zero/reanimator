fdfa799391f94a377ae438f8073e615bf5cd3315
KiteDX <KiteDX@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-01-23 20:11:59 +0000

    Corrected a small error

--- trunk/XML_Test/Program.cs
+++ trunk/XML_Test/Program.cs
@@ -13,9 +13,17 @@ namespace XML_Test
     {
       Revival revival = new Revival();
       Modification mod = revival.AddNewModification("Revival SP modification", "1.2.0", "This is the base SP modification developed by the Revival team.");
-      Index index = mod.AddNewIndexFile("hellgate000");
-      File file = index.AddNewCookedFile("gameglobals.txt.cooked");
-      file.AddNewModifyEntry(1, 1, 20);
+      Index index1 = mod.AddNewIndexFile("hellgate000");
+      File file1 = index1.AddNewCookedFile("gameglobals.txt.cooked");
+      file1.AddNewModifyEntry(1, 1, 20);
+      File file2 = index1.AddNewCookedFile("gamestats.txt.cooked");
+      file2.AddNewModifyEntry(1, 2, 15);
+      file2.AddNewModifyEntry(10, 3, 2);
+      Index index2 = mod.AddNewIndexFile("hellgate123");
+      File file3 = index2.AddNewCookedFile("abc.txt.cooked");
+      file3.AddNewModifyEntry(0, 0, 12);
+      file3.AddNewModifyEntry(3, 5, 1);
+      file2.AddNewModifyEntry(4, 2, 43);
 
       Utilities.Serialize(revival, Directory.GetCurrentDirectory() + @"\Revival.xml");
 
