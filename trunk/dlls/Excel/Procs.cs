58eeb998e2c02a4be64110017050076300a97865
alex.waters <alex.waters@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-02 14:13:49 +0000

    Added Difficulty excel def. Added Levels excel def. Added Procs excel def. Updated the Items excel def. Removed some always -1 or 0 columns from view to speed things up, and specified a few more string attributes.

---
+++ trunk/dlls/Excel/Procs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class Procs : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class ProcsTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public String proc;
+
+            public Int32 id;
+            public Int32 unknown1;
+            public float unknown2;
+            public Int32 unknown3;
+            public float unknown4;
+            public Int32 unknown5;
+            public Int32 unknown6;
+            public Int32 unknown7;
+            public Int32 unknown8;
+        }
+
+        public Procs(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<ProcsTable>(data, ref offset, Count);
+        }
+    }
+}
