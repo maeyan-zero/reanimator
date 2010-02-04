ed4198004eafe1436a7af43e2e6e795f2ed7f5b5
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-04 04:37:47 +0000

    Added DamageTypes table, and updated excelTables.

---
+++ trunk/dlls/Excel/DamageTypes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+namespace Reanimator.Excel
+{
+    public class DamageTypes : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class DamageTypesTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string stat;
+            public short code;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string miniIcon;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string String;
+            short undefined1;
+            public Int32 color;
+            public Int32 shieldHit;
+            public Int32 criticalState;
+            public Int32 softHitState;
+            public Int32 mediumHitState;
+            public Int32 bigHitState;
+            public Int32 fumbleHitState;
+            public Int32 damageOverTimeState;
+            public Int32 fieldMissile;
+            public Int32 invulnerableState;
+            public Int32 invulnerableSfxState;
+            public Int32 thornsState;
+            public Int32 vulnerabilityInPVPTugboat;
+            public Int32 vulnerabilityInPVPHellgate;
+        }
+
+        public DamageTypes(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<DamageTypesTable>(data, ref offset, Count);
+        }
+    }
+}
--- trunk/dlls/Excel/ExcelTables.cs
+++ trunk/dlls/Excel/ExcelTables.cs
@@ -134,6 +134,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("AICOMMONSTATE", null, typeof(Excel.AiCommonState));
             excelTables.AddTable("BOOKMARKS", null, typeof(Excel.AiCommonState));
             excelTables.AddTable("CONDITIONFUNCTIONS", null, typeof(Excel.ConditionFunctions));
+            excelTables.AddTable("DAMAGETYPES", null, typeof(Excel.DamageTypes));
             excelTables.AddTable("DIFFICULTY", null, typeof(Excel.Difficulty));
             excelTables.AddTable("FILTER_CHATFILTER", "CHATFILTER", typeof(Excel.Filter));
             excelTables.AddTable("FILTER_NAMEFILTER", "NAMEFILTER", typeof(Excel.Filter));
