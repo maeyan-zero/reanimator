628038c492f1103b85405c6c94db5a181d2c5b6c
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-04 11:29:49 +0000

    Added UnitEvents table, and updated ExcelTables.

---
+++ trunk/dlls/Excel/UnitEvents.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class UnitEvents : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class UnitEventsTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public String name;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            byte unknown2;
+            public Int32 recordPlayer;
+            public Int32 recordMonster;
+            public Int32 recordMissile;
+            public Int32 recordItem;
+            public Int32 recordObject;
+        }
+
+        public UnitEvents(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<UnitEventsTable>(data, ref offset, Count);
+        }
+    }
+}
