64086e55e2832864a9cee4093c39397c2f82aa29
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-04 11:19:22 +0000

    AddedUIComponent table, and updated ExcelTables.

---
+++ dlls/Excel/UIComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class UIComponent : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class UIComponentTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string script;
+        }
+
+        public UIComponent(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<UIComponentTable>(data, ref offset, Count);
+        }
+    }
+}
