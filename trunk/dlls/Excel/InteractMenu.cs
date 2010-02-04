17d8f5ecbd5eb31de94107b03d9adceb33e5e201
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-04 06:27:24 +0000

    Added InteractMenu table, and updated ExcelTables

---
+++ trunk/dlls/Excel/InteractMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class InteractMenu : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class InteractMenuTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+            public Int32 interaction;//index
+            public Int32 stringTitle;//stridx
+            public Int32 stringToolTip;//stridx
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string frameIcon;
+            public Int32 menuButton;
+
+        }
+
+        public InteractMenu(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<InteractMenuTable>(data, ref offset, Count);
+        }
+    }
+}
