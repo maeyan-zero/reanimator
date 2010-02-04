b13de45c95cc153e7e7e3cfbccb01a91c95fc8cd
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-04 12:50:42 +0000

    added UnitModes table, fixed ConditionFunctions, updated excelTables and excel.csproj

---
+++ dlls/Excel/UnitModes.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class UnitModes : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class UnitModesTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+            [ExcelTables.ExcelOutput(IsStringOffset = true)]
+            public Int32 mode;//pchar
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            byte undefined1;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public Int32[] block;
+            public Int32 blockOnGround;//bool
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public Int32[] wait;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public Int32[] clear;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public Int32[] endBlock;
+            public Int32 group;//idx
+            public Int32 otherhand;//idx
+            public Int32 backUp;//idx
+            public Int32 forceClear;//bool
+            public Int32 clearAi;//bool
+            public Int32 clearSkill;//bool
+            public Int32 clearState;//idx
+            public Int32 clearStateEnd;//idx
+            public Int32 doEvent;//bool
+            public Int32 endEvent;//bool
+            public Int32 doFunction;//pchar
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+            public Int32[] undefined2;
+            public Int32 clearFunction;//pchar
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+            public Int32[] undefined3;
+            public Int32 endFunction;//pchar
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+            public Int32[] undefined4;
+            public Int32 endMode;//idx
+            public Int32 velocityName;//idx?
+            public Int32 velocityPriority;
+            public Int32 animPriority;
+            public Int32 loadPriorityPercent;
+            public Int32 velocityUsesMeleeSpeed;//bool
+            public Int32 velocityChangedByStats;//bool
+            public Int32 noLoop;//bool
+            public Int32 restoreAi;//bool
+            public Int32 steps;//bool
+            public Int32 lazyEndForControlUnit;//bool
+            public Int32 noAnimation;//bool
+            public Int32 ragdoll;//bool
+            public Int32 playRight;//bool
+            public Int32 playLeft;//bool
+            public Int32 playTorso;//bool
+            public Int32 playLegs;//bool
+            public Int32 playJustDefault;//bool
+            public Int32 isAggressive;//bool
+            public Int32 playAllVariations;//bool
+            public Int32 resetMixableOnStart;//bool
+            public Int32 resetMixableOnEnd;//bool
+            public Int32 randStartTime;//bool
+            public Int32 durationFromAnims;//bool
+            public Int32 durationFromContact;//bool
+            public Int32 clearAdjustStance;//bool
+            public Int32 checkCanInterrupt;//bool
+            public Int32 useBackupModeAnims;//bool
+            public Int32 playOnInventoryModel;//bool
+            public Int32 undefined5;
+
+
+        }
+
+        public UnitModes(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<UnitModesTable>(data, ref offset, Count);
+        }
+    }
+}
