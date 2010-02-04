b78edcd8f5e21940581d673ac22c6d0c7eaaedce
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-04 04:22:58 +0000

    Added ConditionFunctions table, and updated excelTables.

---
+++ dlls/Excel/ConditionFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class ConditionFunctions : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class ConditionFunctionsTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string function;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+            byte undefined1;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 4)]
+            public Int16 undefinedBool;
+            public Int16 usesMonsterClass;//bool
+            public Int16 usesObjectClass;//bool
+            public Int16 usesStat;//bool
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string paramText0;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string paramText1;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 4)]
+            byte undefined2;
+        }
+
+        public ConditionFunctions(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<ConditionFunctionsTable>(data, ref offset, Count);
+        }
+
+    }
+}
