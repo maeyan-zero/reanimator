87b8840eaaaa5aac91e8d97e5c0a975809f770a9
alex.waters <alex.waters@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-04 10:51:42 +0000

    Forgot Properties.cs

---
+++ dlls/Excel/Properties.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class Properties : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public class PropertiesTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public Int32[] header;
+            public Int32 property;
+        }
+
+        public Properties(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<PropertiesTable>(data, ref offset, Count);
+        }
+    }
+}
