a9ce56a461c146b6328f08209f591e24bb9ff4c4
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-04 11:38:06 +0000

    Added UnitModeGroups table, and updated ExcelTables.

---
+++ dlls/Excel/UnitModeGroups.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class UnitModeGroups : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class UnitModeGroupsTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public String name;
+
+            public Int32 code;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 7)]
+            public Int32 undefined;
+        }
+
+        public UnitModeGroups(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<UnitModeGroupsTable>(data, ref offset, Count);
+        }
+    }
+}
