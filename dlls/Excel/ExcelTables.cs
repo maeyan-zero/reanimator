79fc9cdff3fb4e8e6ae1f991c7eab411254f0b03
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 07:27:44 +0000

    Added AnimationStance table and updated ExcelTables.

--- dlls/Excel/ExcelTables.cs
+++ dlls/Excel/ExcelTables.cs
@@ -140,6 +140,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("AISTART", null, typeof(Excel.AiStart));
             excelTables.AddTable("ANIMATIONCONDITION", null, typeof(Excel.AnimationCondition));
             excelTables.AddTable("ANIMATIONGROUPS", null, typeof(Excel.AnimationGroups));
+            excelTables.AddTable("ANIMATIONSTANCE", null, typeof(Excel.AnimationStance));
             excelTables.AddTable("BOOKMARKS", null, typeof(Excel.BookMarks));
             excelTables.AddTable("CONDITION_FUNCTIONS", null, typeof(Excel.ConditionFunctions));
             excelTables.AddTable("DAMAGETYPES", null, typeof(Excel.DamageTypes));
