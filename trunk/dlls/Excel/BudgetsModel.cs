06a7d21e36dcde122e49482677874f40383773a1
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 08:07:48 +0000

    Added BudgetsModel table, and updated ExcelTables.

---
+++ trunk/dlls/Excel/BudgetsModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class BudgetsModel : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class BudgetsModelTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            public Int32 group;
+            public float lodRate;
+        }
+
+        public BudgetsModel(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<BudgetsModelTable>(data, ref offset, Count);
+        }
+    }
+}
