b5a1e08430a379b888badec01d31fade31b4f8ed
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 08:55:42 +0000

    added ColorSets table, and updated ExcelTables.

---
+++ trunk/dlls/Excel/ColorSets.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class ColorSets : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class ColorSetsTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string color;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+            Int32[] undefined;
+            public Int32 code;
+            public Int32 canBeRandomPick;//bool
+        }
+
+        public ColorSets(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<ColorSetsTable>(data, ref offset, Count);
+        }
+    }
+}
