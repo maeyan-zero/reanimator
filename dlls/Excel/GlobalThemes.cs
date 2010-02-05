c95d9436eba0ef665eb31fc386cb5e84badcf90d
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 11:30:48 +0000

    added GlobalThemes table, and updated ExcelTables.

---
+++ dlls/Excel/GlobalThemes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class GlobalThemes : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class GlobalThemesTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string name;
+            public Int32 startMonth;
+            public Int32 startDay;
+            public Int32 startDayOfWeek;
+            public Int32 endMonth;
+            public Int32 endDay;
+            public Int32 endDayOfWeek;
+            public Int32 treasureClassPreAndPost0a;
+            public Int32 treasureClassPreAndPost0b;
+            public Int32 treasureClassPreAndPost1a;
+            public Int32 treasureClassPreAndPost1b;
+            public Int32 treasureClassPreAndPost2a;
+            public Int32 treasureClassPreAndPost2b;
+            public Int32 treasureClassPreAndPost3a;
+            public Int32 treasureClassPreAndPost3b;
+            public Int32 treasureClassPreAndPost4a;
+            public Int32 treasureClassPreAndPost4b;
+            public Int32 treasureClassPreAndPost5a;
+            public Int32 treasureClassPreAndPost5b;
+            public Int32 treasureClassPreAndPost6a;
+            public Int32 treasureClassPreAndPost6b;
+            public Int32 treasureClassPreAndPost7a;
+            public Int32 treasureClassPreAndPost7b;
+            public Int32 activateByTime;
+        }
+
+        public GlobalThemes(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<GlobalThemesTable>(data, ref offset, Count);
+        }
+    }
+}
