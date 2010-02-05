5985bf24b20ff277675e97d3296219e948943d16
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 06:37:18 +0000

    Added Act table, and updated ExcelTables

---
+++ trunk/dlls/Excel/Act.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class Act : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class ActTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+
+            public Int32 code;
+            public Int32 bitmask;/*0 bit beta account can play
+	1 bit non_subscriber account can play*/
+        }
+
+        public Act(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<ActTable>(data, ref offset, Count);
+        }
+    }
+}
