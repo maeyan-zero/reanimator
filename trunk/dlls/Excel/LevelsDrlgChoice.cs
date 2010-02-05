3da1f775054b7891ee2355a33445a963977b26e6
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 11:55:37 +0000

    added LevelsDrlgChoice table, and updated ExcelTables.

---
+++ trunk/dlls/Excel/LevelsDrlgChoice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class LevelsDrlgChoice : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class LevelsDrlgChoiceTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+            [ExcelTables.ExcelOutput(IsStringOffset = true)]
+            public Int32 name; //pchar
+            public Int32 undefined1;
+            public float namedMonsterChance;
+            public Int32 undefined2;
+            public Int32 levelName;//idx
+            public Int32 difficulty;//idx
+            public Int32 drlg;//idx
+            public Int32 spawnClass;//idx
+            public Int32 namedMonsterClass;//idx
+            public Int32 weight;
+            public Int32 music;//idx
+            public Int32 environmentOverRide;//idx
+            public float environmentSpawnClassRoomDensity;
+            public Int32 environmentSpawnClass;//idx
+            public Int32 code;
+            public Int32 undefined3;
+        }
+
+        public LevelsDrlgChoice(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<LevelsDrlgChoiceTable>(data, ref offset, Count);
+        }
+    }
+}
