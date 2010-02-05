7731d602a91ef7d7ec930ee65cec27f8cf274476
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 13:18:20 +0000

    added LevelScaling and updated ExcelTables.

---
+++ trunk/dlls/Excel/LevelScaling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class LevelScaling : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class LevelScalingTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            public Int32 levelDiff;
+            public Int32 PlayerAttackMonsterDmg;
+            public Int32 PlayerAttackMonsterExp;
+            public Int32 MonsterAttackPlayerDmg;
+            public Int32 PlayerAttackPlayerDmg;
+        }
+
+        public LevelScaling(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<LevelScalingTable>(data, ref offset, Count);
+        }
+    }
+}
