0990e726c83f1bb15a41a6ebfe3de60ed1245fa9
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 07:33:32 +0000

    Added BackGroundSounds table, and updated ExcelTables.

---
+++ trunk/dlls/Excel/BackGroundSounds.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class BackGroundSounds : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class BackGroundSoundsTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+
+            public Int32 sounds2D0;
+            public Int32 sounds2D1;
+            public Int32 sounds2D2;
+            public Int32 sounds2D3;
+            public Int32 sounds2D4;
+            public Int32 sounds2D5;
+            public Int32 sounds2D6;
+            public Int32 sounds2D7;
+            public Int32 sounds2D8;
+            public Int32 sounds2D9;
+            public Int32 sounds3D0;
+            public Int32 sounds3D1;
+            public Int32 sounds3D2;
+            public Int32 sounds3D3;
+            public Int32 sounds3D4;
+            public Int32 sounds3D5;
+            public Int32 sounds3D6;
+            public Int32 sounds3D7;
+            public Int32 sounds3D8;
+            public Int32 sounds3D9;
+        }
+
+        public BackGroundSounds(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<BackGroundSoundsTable>(data, ref offset, Count);
+        }
+    }
+}
