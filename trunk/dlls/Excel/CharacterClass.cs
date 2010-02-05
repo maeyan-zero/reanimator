d7004b474355c6ce2c97ba2fc47762b13b9af6c3
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 08:48:33 +0000

    added CharacterClass table, and updated ExcelTables.

---
+++ trunk/dlls/Excel/CharacterClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class CharacterClass : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class CharacterClassTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+
+            public Int32 maleUnit0;
+            public Int32 maleUnit1;
+            public Int32 maleUnit2;
+            public Int32 maleUnit3;
+            public Int32 maleUnit4;
+            public Int32 maleUnit5;
+            public Int32 maleUnit6;
+            public Int32 maleUnit7;
+            public Int32 maleEnabled;//bool
+            public Int32 femaleUnit0;
+            public Int32 femaleUnit1;
+            public Int32 femaleUnit2;
+            public Int32 femaleUnit3;
+            public Int32 femaleUnit4;
+            public Int32 femaleUnit5;
+            public Int32 femaleUnit6;
+            public Int32 femaleUnit7;
+            public Int32 femaleEnabled;//bool
+            public Int32 unitVersionToGetSkillRespec;
+            public Int32 stringOneLetterCode;//stridx
+            public Int32 Default;//bool
+            public Int32 scrapItemClassSpecial;//idx
+        }
+
+        public CharacterClass(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<CharacterClassTable>(data, ref offset, Count);
+        }
+    }
+}
--- trunk/dlls/Excel/ExcelTables.cs
+++ trunk/dlls/Excel/ExcelTables.cs
@@ -150,6 +150,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("BOOKMARKS", null, typeof(Excel.BookMarks));
             excelTables.AddTable("BUDGETSMODEL", null, typeof(Excel.BudgetsModel));
             excelTables.AddTable("BUDGETTEXTUREMIPS", null, typeof(Excel.BudgetTextureMips));
+            excelTables.AddTable("CHARACTERCLASS", null, typeof(Excel.CharacterClass));
             excelTables.AddTable("CONDITION_FUNCTIONS", null, typeof(Excel.ConditionFunctions));
             excelTables.AddTable("DAMAGETYPES", null, typeof(Excel.DamageTypes));
             excelTables.AddTable("DIALOG", null, typeof(Excel.Dialog));
