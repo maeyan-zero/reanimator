cbaf6a8d32b9250e792d250c314548de06a06af1
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 12:26:32 +0000

    added LevelsEnv table, and updated ExcelTables.

---
+++ dlls/Excel/LevelsEnv.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class LevelsEnv : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class LevelsEnvTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+
+            public Int32 undefined;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string fileName;
+        }
+
+        public LevelsEnv(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<LevelsEnvTable>(data, ref offset, Count);
+        }
+    }
+}
