5ec920817d59468b2a6663ffb02f9cb94920423d
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 07:44:31 +0000

    Added BackGroundSounds2D table, and updated ExcelTables.

--- dlls/Excel/ExcelTables.cs
+++ dlls/Excel/ExcelTables.cs
@@ -142,6 +142,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("ANIMATIONGROUPS", null, typeof(Excel.AnimationGroups));
             excelTables.AddTable("ANIMATIONSTANCE", null, typeof(Excel.AnimationStance));
             excelTables.AddTable("BACKGROUNDSOUNDS", null, typeof(Excel.BackGroundSounds));
+            excelTables.AddTable("BACKGROUNDSOUNDS2D", null, typeof(Excel.BackGroundSounds2D));
             excelTables.AddTable("BOOKMARKS", null, typeof(Excel.BookMarks));
             excelTables.AddTable("CONDITION_FUNCTIONS", null, typeof(Excel.ConditionFunctions));
             excelTables.AddTable("DAMAGETYPES", null, typeof(Excel.DamageTypes));
