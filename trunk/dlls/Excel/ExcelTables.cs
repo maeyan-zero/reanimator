c7513b1e35c99c806e7ac0755178cbc737bd2bd7
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 07:59:47 +0000

    Added BadgeRewards table, and updated ExcelTables.

--- trunk/dlls/Excel/ExcelTables.cs
+++ trunk/dlls/Excel/ExcelTables.cs
@@ -144,6 +144,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("BACKGROUNDSOUNDS", null, typeof(Excel.BackGroundSounds));
             excelTables.AddTable("BACKGROUNDSOUNDS2D", null, typeof(Excel.BackGroundSounds2D));
             excelTables.AddTable("BACKGROUNDSOUNDS3D", null, typeof(Excel.BackGroundSounds3D));
+            excelTables.AddTable("BADGEREWARDS", null, typeof(Excel.BadgeRewards));
             excelTables.AddTable("BOOKMARKS", null, typeof(Excel.BookMarks));
             excelTables.AddTable("CONDITION_FUNCTIONS", null, typeof(Excel.ConditionFunctions));
             excelTables.AddTable("DAMAGETYPES", null, typeof(Excel.DamageTypes));
