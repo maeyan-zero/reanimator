4993d98ab774e17532672834cd9e8ff9bf42973a
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 11:40:06 +0000

    added InitDb table, and updated ExcelTables.

---
+++ trunk/dlls/Excel/InitDb.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class InitDb : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class InitDbTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            public Int32 skip;//bool
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 4)]
+            public string criteria;
+
+            public Int32 rangeLow;
+            public Int32 rangeHigh;
+            public float numMin;
+            public float numMax;
+            public float numInit;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 4)]
+            public string featKnob;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 4)]
+            public string featMin;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 4)]
+            public string featMax;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 4)]
+            public string featInit;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 4)]
+            public string numKnob;
+
+        }
+
+        public InitDb(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<InitDbTable>(data, ref offset, Count);
+        }
+    }
+}
