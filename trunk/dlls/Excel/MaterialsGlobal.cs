9e2d19b0c9521b1baaac0a379269df375b4895f8
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 13:49:47 +0000

    added MaterialsGlobal table, updated ExcelTables.

---
+++ trunk/dlls/Excel/MaterialsGlobal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class MaterialsGlobal : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class MaterialsGlobalTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string material;
+            public Int32 undefined;
+        }
+
+        public MaterialsGlobal(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<MaterialsGlobalTable>(data, ref offset, Count);
+        }
+    }
+}
