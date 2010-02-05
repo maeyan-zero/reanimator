37c1b308b0ad3ef9269f6e4a2d05e233a1a1f207
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 07:27:44 +0000

    Added AnimationStance table and updated ExcelTables.

---
+++ trunk/dlls/Excel/AnimationStance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class AnimationStance : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class AnimationStanceTable
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+
+            public Int32 dontChangeFrom;//bool
+        }
+
+        public AnimationStance(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<AnimationStanceTable>(data, ref offset, Count);
+        }
+    }
+}
