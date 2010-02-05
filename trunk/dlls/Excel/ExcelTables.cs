5e6752c6181186e668b605f0278057cb23ed7018
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-05 07:24:08 +0000

    Added AnimationGroups table, and updated ExcelTables.

--- trunk/dlls/Excel/ExcelTables.cs
+++ trunk/dlls/Excel/ExcelTables.cs
@@ -139,6 +139,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("AIINIT", null, typeof(Excel.AiInit));
             excelTables.AddTable("AISTART", null, typeof(Excel.AiStart));
             excelTables.AddTable("ANIMATIONCONDITION", null, typeof(Excel.AnimationCondition));
+            excelTables.AddTable("ANIMATIONGROUPS", null, typeof(Excel.AnimationGroups));
             excelTables.AddTable("BOOKMARKS", null, typeof(Excel.BookMarks));
             excelTables.AddTable("CONDITION_FUNCTIONS", null, typeof(Excel.ConditionFunctions));
             excelTables.AddTable("DAMAGETYPES", null, typeof(Excel.DamageTypes));
