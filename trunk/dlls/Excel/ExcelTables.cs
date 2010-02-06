da9b7ecb5787a291cf0d3ab483bf6883253654a7
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-06 05:02:45 +0000

    added Music table, and updated ExcelTables.

--- trunk/dlls/Excel/ExcelTables.cs
+++ trunk/dlls/Excel/ExcelTables.cs
@@ -206,6 +206,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("MOVIE_SUBTITLES", "MOVIESUBTITLES", typeof(Excel.MovieSubTitles));
             excelTables.AddTable("MOVIELISTS", null, typeof(Excel.MovieLists));
             excelTables.AddTable("MOVIES", null, typeof(Excel.Movies));
+            excelTables.AddTable("MUSIC", null, typeof(Excel.Music));
             excelTables.AddTable("OBJECTS", null, typeof(Excel.Items));
             excelTables.AddTable("PLAYERS", null, typeof(Excel.Items));
             excelTables.AddTable("PROPERTIES", null, typeof(Excel.Properties));
