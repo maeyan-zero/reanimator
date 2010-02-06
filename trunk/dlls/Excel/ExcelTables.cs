8094341344c5203b83e140e2783d8ec5b4e6d8b7
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-06 06:53:58 +0000

    added MusicStingerSets table, and updated ExcelTables.

--- trunk/dlls/Excel/ExcelTables.cs
+++ trunk/dlls/Excel/ExcelTables.cs
@@ -212,6 +212,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("MUSICREF", null, typeof(Excel.MusicRef));
             excelTables.AddTable("MUSICSCRIPTDEBUG", null, typeof(Excel.MusicScriptDebug));
             excelTables.AddTable("MUSICSTINGERS", null, typeof(Excel.MusicStingers));
+            excelTables.AddTable("MUSICSTINGERSETS", null, typeof(Excel.MusicStingerSets));
             excelTables.AddTable("OBJECTS", null, typeof(Excel.Items));
             excelTables.AddTable("PLAYERS", null, typeof(Excel.Items));
             excelTables.AddTable("PROPERTIES", null, typeof(Excel.Properties));
