b944eb13dbf5b7daa419733cfd7e9f9110f48cd8
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-06 06:31:03 +0000

    added MusicScriptDebug table, and updated ExcelTables.

--- trunk/dlls/Excel/ExcelTables.cs
+++ trunk/dlls/Excel/ExcelTables.cs
@@ -210,6 +210,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("MUSICGROOVELEVELS", null, typeof(Excel.MusicGrooveLevels));
             excelTables.AddTable("MUSICGROOVELEVELTYPES", null, typeof(Excel.MusicGrooveLevelTypes));
             excelTables.AddTable("MUSICREF", null, typeof(Excel.MusicRef));
+            excelTables.AddTable("MUSICSCRIPTDEBUG", null, typeof(Excel.MusicScriptDebug));
             excelTables.AddTable("OBJECTS", null, typeof(Excel.Items));
             excelTables.AddTable("PLAYERS", null, typeof(Excel.Items));
             excelTables.AddTable("PROPERTIES", null, typeof(Excel.Properties));
