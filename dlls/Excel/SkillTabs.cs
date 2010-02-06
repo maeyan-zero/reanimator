e74eb0fdf6a7d8504a4fba19597e090648e78957
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-06 11:04:14 +0000

    added SkillTabs table, and updated ExcelTables.

---
+++ dlls/Excel/SkillTabs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class SkillTabs : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class SkillTabsTable
+        {
+            TableHeader header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+
+            public Int32 code;
+            public Int32 displayString;//stridx
+            public Int32 drawOnlyKnown;//bool
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string iconTextureName;
+        }
+
+        public SkillTabs(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<SkillTabsTable>(data, ref offset, Count);
+        }
+    }
+}
