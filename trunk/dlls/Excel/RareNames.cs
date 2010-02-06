29609788e09c8a272e770094c046089418ac3c25
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-06 10:44:28 +0000

    added RareNames table, and updated ExcelTables.

---
+++ trunk/dlls/Excel/RareNames.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class RareNames : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class RareNamesTable
+        {
+            TableHeader header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+
+            public Int32 suffix;//bool
+            public Int32 level;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 4)]
+            public string code;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public Int32[] types;
+            public Int32 cond;//intptr
+        }
+
+        public RareNames(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<RareNamesTable>(data, ref offset, Count);
+        }
+    }
+}
