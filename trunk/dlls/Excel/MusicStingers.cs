82fe853c418c7534626c90b8195c96ee5c498ee3
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-06 06:35:15 +0000

    added MusicStingers table, and updated ExcelTables.

---
+++ trunk/dlls/Excel/MusicStingers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Reanimator.Excel
+{
+    public class MusicStingers : ExcelTable
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class MusicStingersTable
+        {
+            TableHeader header;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string name;
+
+            public Int32 type;
+            public Int32 fadeOutBeats;
+            public Int32 fadeInBeats;
+            public Int32 fadeInDelayBeats;
+            public Int32 fadeOutDelayBeats;
+            public Int32 introBeats;
+            public Int32 outroBeats;
+            public Int32 soundGroup;//idx
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            Int32[] undefined;
+        }
+
+        public MusicStingers(byte[] data) : base(data) { }
+
+        protected override void ParseTables(byte[] data)
+        {
+            ReadTables<MusicStingersTable>(data, ref offset, Count);
+        }
+    }
+}
