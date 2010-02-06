d3121addbb3a7910f7682ab855a98616962a480c
malachor@bigpond.net.au <malachor@bigpond.net.au@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-06 11:29:52 +0000

    added SoundMixStateValues table, and updated ExcelTables.

--- dlls/Excel/ExcelTables.cs
+++ dlls/Excel/ExcelTables.cs
@@ -240,6 +240,7 @@ namespace Reanimator.Excel
             excelTables.AddTable("SKU", null, typeof(Excel.Sku));
             excelTables.AddTable("SOUNDBUSES", null, typeof(Excel.SoundBuses));
             excelTables.AddTable("SOUNDMIXSTATES", null, typeof(Excel.SoundMixStates));
+            excelTables.AddTable("SOUNDMIXSTATEVALUES", null, typeof(Excel.SoundMixStateValues));
             excelTables.AddTable("STATE_EVENT_TYPES", null, typeof(Excel.StateEventTypes));
             excelTables.AddTable("STATE_LIGHTING", null, typeof(Excel.StateLighting));
             excelTables.AddTable("STATES", null, typeof(Excel.States));
