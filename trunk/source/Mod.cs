fc6b424f8eec71915169316d3f1829a0fe5aa399
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-07 12:16:20 +0000

    Just a general backup of the changes made to the mod class.

--- trunk/source/Mod.cs
+++ trunk/source/Mod.cs
@@ -12,9 +12,9 @@ namespace Reanimator
 {
 #pragma warning disable 0169
 #pragma warning disable 0649
-    static class Mod
+    public class Mod
     {
-        private static List<Modification> modification;
+        List<Modification> modification;
 
         public static bool Parse(string xml)
         {
@@ -86,6 +86,11 @@ namespace Reanimator
 
             [XmlElement("pack")]
             public Pack[] pack;
+
+            public Modification()
+            {
+
+            }
         }
 
         public class Pack
@@ -103,7 +108,7 @@ namespace Reanimator
             public string id;
 
             [XmlElement("modify")]
-            public Modify[] modify;
+            public Modify modify;
         }
 
         public class Modify
@@ -127,12 +132,50 @@ namespace Reanimator
             public string id;
 
             [XmlElement("replace")]
-            public object replace;
+            public object[] replace;
 
-            [XmlElement("bitwise")]
-            public bool bitwise;
+            [XmlElement("bitmask")]
+            public bool[] bitmask;
+        }
+
+        public class Replace
+        {
+            public object replace;
+        }
+
+        public void DemoMod()
+        {
+            Revival revival = new Revival();
+
+            revival.modification = new Modification[1];
+            revival.modification[0] = new Modification();
+            revival.modification[0].title = "Revival SP modificaton";
+            revival.modification[0].description = "This is the description";
+            revival.modification[0].version = "1.1.0";
+            revival.modification[0].pack = new Pack[1];
+            revival.modification[0].pack[0] = new Pack();
+            revival.modification[0].pack[0].id = "hellgate000";
+            revival.modification[0].pack[0].file = new File[1];
+            revival.modification[0].pack[0].file[0] = new File();
+            revival.modification[0].pack[0].file[0].id = "gameglobals.txt.cooked";
+            revival.modification[0].pack[0].file[0].modify = new Modify();
+            revival.modification[0].pack[0].file[0].modify.entity = new Entity[1];
+            revival.modification[0].pack[0].file[0].modify.entity[0] = new Entity();
+            revival.modification[0].pack[0].file[0].modify.entity[0].id = "vendorRefresh";
+            revival.modification[0].pack[0].file[0].modify.entity[0].attribute = new Attribute[1];
+            revival.modification[0].pack[0].file[0].modify.entity[0].attribute[0] = new Attribute();
+            revival.modification[0].pack[0].file[0].modify.entity[0].attribute[0].id = "intData";
+            revival.modification[0].pack[0].file[0].modify.entity[0].attribute[0].replace = new String[1];
+            revival.modification[0].pack[0].file[0].modify.entity[0].attribute[0].replace[0] = "1123";
+
+            // Serialization
+            XmlSerializer s = new XmlSerializer(typeof(Revival));
+            TextWriter w = new StreamWriter(@"c:\list.xml");
+            s.Serialize(w, revival);
+            w.Close();
         }
     }
+
 #pragma warning restore 0169
 #pragma warning restore 0649
 }
