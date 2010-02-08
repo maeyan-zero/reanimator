dbb07bebe89128df5587d78acee07079840d9f32
alex.waters <alex.waters@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-02-08 09:19:31 +0000

    Opps, forgot this.

---
+++ trunk/source/Forms/ThreadedFormBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Reanimator.Forms
+{
+    public class ThreadedFormBase : Form
+    {
+        delegate DialogResult ShowDialogCallback(CommonDialog dialog);
+        protected DialogResult ShowDialog(CommonDialog dialog)
+        {
+            if (this.InvokeRequired)
+            {
+                ShowDialogCallback d = new ShowDialogCallback(ShowDialog);
+                return (DialogResult)this.Invoke(d, new Object[] { dialog });
+            }
+            else
+            {
+                return dialog.ShowDialog(this);
+            }
+        }
+
+        delegate void DisposeCallback();
+        new protected void Dispose()
+        {
+            if (this.InvokeRequired)
+            {
+                DisposeCallback d = new DisposeCallback(Dispose);
+                this.Invoke(d);
+            }
+            else
+            {
+                base.Dispose();
+            }
+        }
+    }
+}
