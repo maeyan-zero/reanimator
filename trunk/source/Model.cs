a74ee376619f3bbe4d7068a29ebdf355a6c25666
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-03-16 09:02:36 +0000

    More Havok reversed

--- trunk/source/Model.cs
+++ trunk/source/Model.cs
@@ -8,26 +8,51 @@ using System.Xml;
 
 namespace Reanimator
 {
-    class Model
+    public class Model
     {
-        String modelID;
+        String _modelID;
 
         Int32 type;
         Int32 minorVersion;
         Int32 majorVersion;
         Table[] fileStructure;
 
-        List<Index> index;
-        List<Table> indexMap;
-        List<Geometry> geometry;
-        Reserved reserved;
-        Footer footer;
+        List<Index> _index;
+        List<Table> _indexMap;
+        List<Geometry> _geometry;
+        Reserved _reserved;
+        Footer _footer;
+
+        public string id
+        {
+            get
+            {
+                return _modelID;
+            }
+        }
+
+        public List<Index> index
+        {
+            get
+            {
+                return _index;
+            }
+        }
+
+        public List<Geometry> geometry
+        {
+            get
+            {
+                return _geometry;
+            }
+        }
 
         public Model(BinaryReader binReader)
         {
-            index = new List<Index>();
-            indexMap = new List<Table>();
-            geometry = new List<Geometry>();
+            _modelID = "model";
+            _index = new List<Index>();
+            _indexMap = new List<Table>();
+            _geometry = new List<Geometry>();
 
             type = binReader.ReadInt32();
             minorVersion = binReader.ReadInt32();
@@ -41,17 +66,17 @@ namespace Reanimator
                 switch (structure.id)
                 {
                     case (uint)Structure.UnitIndex:
-                        index.Add(GetIndex(binReader));
+                        _index.Add(GetIndex(binReader));
                         break;
                     case (uint)Structure.Geometry:
-                        geometry.Add(GetGeometry(binReader));
+                        _geometry.Add(GetGeometry(binReader));
                         break;
                     case (uint)Structure.Reserved:// skip this for now
                         binReader.ReadBytes((int)structure.size); 
                         //reserved = GetReserved(binReader);
                         break;
                     case (uint)Structure.Footer:
-                        footer = GetFooter(binReader);
+                        _footer = GetFooter(binReader);
                         break;
                 }
             }
@@ -172,365 +197,10 @@ namespace Reanimator
         }
 
         //
-        // MISC HELPER FUNCTIONS:
-        //
-
-        public Single[] MergePositions()
-        {
-            int length = 0;
-            foreach (Geometry g in geometry)
-            {
-                length += g.position.Length * 3;
-            }
-
-            Single[] positions = new Single[length];
-            length = 0;
-            foreach (Geometry g in geometry)
-            {
-                for (int i = 0; i < g.position.Length; i++)
-                {
-                    positions[length++] = g.position[i].positionX;
-                    positions[length++] = g.position[i].positionY;
-                    positions[length++] = g.position[i].positionZ;
-                }
-            }
-
-            return positions;
-        }
-
-        public UInt16[] GetTriangleArray(int idx)
-        {
-            int length = 0;
-            UInt16[] triangles = new UInt16[index[idx].triangles * 3];
-            
-            for (int i = 0; i < triangles.Length / 3; i++)
-            {
-                triangles[length++] = index[idx].triangle[i].coordinate01;
-                triangles[length++] = index[idx].triangle[i].coordinate02;
-                triangles[length++] = index[idx].triangle[i].coordinate03;
-            }
-
-            return triangles;
-        }
-
-        public Single[] GetUVArray(int idx)
-        {
-            int length = 0;
-            Single[] uv = new Single[geometry[idx].uv.Length * 2];
-
-            for (int i = 0; i < uv.Length / 2; i++)
-            {
-                uv[length++] = geometry[idx].uv[i].s;
-                uv[length++] = geometry[idx].uv[i].t;
-            }
-
-            return uv;
-        }
-
-        //
-        // EXPORT COLLADA:
-        //
-
-        public void Export()
-        {
-            XmlWriterSettings wSettings = new XmlWriterSettings();
-            wSettings.Indent = true;
-            MemoryStream ms = new MemoryStream();
-            XmlWriter xw = XmlWriter.Create(ms, wSettings);// Write Declaration
-
-            //debug
-            modelID = "model_name_here";
-            DateTime now = DateTime.Now;
-
-            //
-            // HEADER
-            //
-
-            xw.WriteStartDocument();
-            xw.WriteStartElement("COLLADA");
-            xw.WriteAttributeString("version", "1.4.1");
-            //xw.WriteAttributeString("xmlns", "http://www.collada.org/2005/11/COLLADASchema");
-            xw.WriteStartElement("asset");
-            xw.WriteStartElement("contributor");
-            xw.WriteStartElement("author");
-            xw.WriteString("Ripped by Maeyan");
-            xw.WriteEndElement();
-            xw.WriteStartElement("authoring_tool");
-            xw.WriteString("3ds Max 7.0.0");
-            xw.WriteEndElement();
-            xw.WriteStartElement("copyright");
-            xw.WriteString("Flagship Studios");
-            xw.WriteEndElement();
-            xw.WriteEndElement();// End Contributor
-            xw.WriteStartElement("created");
-            xw.WriteValue(now - new TimeSpan(0, 0, 0));
-            xw.WriteEndElement();
-            xw.WriteStartElement("modified");
-            xw.WriteValue(now - new TimeSpan(0, 0, 0));
-            xw.WriteEndElement();
-            xw.WriteStartElement("unit");
-            xw.WriteAttributeString("meter", "0.01");
-            xw.WriteAttributeString("name", "centimeter");
-            xw.WriteEndElement();
-            xw.WriteStartElement("up_axis");
-            xw.WriteString("Y_UP");
-            xw.WriteEndElement();
-            xw.WriteEndElement();// End asset
-
-            //
-            // MATERIAL
-            //
-
-            xw.WriteStartElement("library_materials");
-            xw.WriteStartElement("material");
-            xw.WriteAttributeString("id", "default");
-            xw.WriteStartElement("instance_effect");
-            xw.WriteAttributeString("url", "#lambert-fx");
-            xw.WriteEndElement();
-            xw.WriteEndElement();
-            xw.WriteEndElement();
-
-            //
-            // EFFECT
-            //
-
-            xw.WriteStartElement("library_effects");
-            xw.WriteStartElement("effect");
-            xw.WriteAttributeString("id", "lambert-fx");
-            xw.WriteStartElement("profile_COMMON");
-            xw.WriteStartElement("technique");
-            xw.WriteAttributeString("sid", "common");
-            xw.WriteStartElement("lambert");
-
-            xw.WriteStartElement("emission");
-            xw.WriteStartElement("color");
-            xw.WriteString("0 0 0 1");
-            xw.WriteEndElement(); // color
-            xw.WriteEndElement(); // diffuse
-
-            xw.WriteStartElement("ambient");
-            xw.WriteStartElement("color");
-            xw.WriteString("0 0 0 1");
-            xw.WriteEndElement(); // color
-            xw.WriteEndElement(); // diffuse
-
-            xw.WriteStartElement("diffuse");
-            xw.WriteStartElement("color");
-            xw.WriteString("0.5 0.5 0.5 1");
-            xw.WriteEndElement(); // color
-            xw.WriteEndElement(); // diffuse
-
-            xw.WriteStartElement("reflective");
-            xw.WriteStartElement("color");
-            xw.WriteString("1 1 1 1");
-            xw.WriteEndElement(); // color
-            xw.WriteEndElement(); // diffuse
-
-            xw.WriteStartElement("reflectivity");
-            xw.WriteStartElement("float");
-            xw.WriteString("1");
-            xw.WriteEndElement(); // color
-            xw.WriteEndElement(); // diffuse
-
-            xw.WriteStartElement("transparent");
-            xw.WriteStartElement("color");
-            xw.WriteString("0 0 0 1");
-            xw.WriteEndElement(); // color
-            xw.WriteEndElement(); // diffuse
-
-            xw.WriteStartElement("transparency");
-            xw.WriteStartElement("float");
-            xw.WriteString("1");
-            xw.WriteEndElement(); // color
-            xw.WriteEndElement(); // diffuse
-
-            xw.WriteStartElement("index_of_refraction");
-            xw.WriteStartElement("float");
-            xw.WriteString("0");
-            xw.WriteEndElement(); // color
-            xw.WriteEndElement(); // diffuse
-
-            xw.WriteEndElement(); // lambert
-            xw.WriteEndElement(); // Technique
-            xw.WriteEndElement(); // profile_COMMON
-            xw.WriteEndElement(); // Effect
-            xw.WriteEndElement(); // Library Effects
-
-            //
-            // LIBRARY GEOMETRIES
-            //
-
-            xw.WriteStartElement("library_geometries");
-            xw.WriteAttributeString("id", modelID);
-            xw.WriteStartElement("geometry");
-            xw.WriteStartElement("mesh");
-            
-            //
-            // POSITIONS
-            //
-
-            Single[] positions = MergePositions();
-
-            xw.WriteStartElement("source");
-            xw.WriteAttributeString("id", modelID + "-positions");
-            xw.WriteStartElement("float_array");
-            xw.WriteAttributeString("id", modelID + "-positions-array");
-            xw.WriteAttributeString("count", positions.Length.ToString());
-            xw.WriteString(FileTools.ArrayToStringGeneric<Single>(positions, " "));
-            xw.WriteEndElement();
-            xw.WriteStartElement("technique_common");
-            xw.WriteStartElement("accessor");
-            xw.WriteAttributeString("stride", "3");
-            xw.WriteAttributeString("source", "#" + modelID + "-positions-array");
-            xw.WriteAttributeString("count", (positions.Length / 3).ToString());
-            xw.WriteStartElement("param");
-            xw.WriteAttributeString("name", "X");
-            xw.WriteAttributeString("type", "float");
-            xw.WriteEndElement();
-            xw.WriteStartElement("param");
-            xw.WriteAttributeString("name", "Y");
-            xw.WriteAttributeString("type", "float");
-            xw.WriteEndElement();
-            xw.WriteStartElement("param");
-            xw.WriteAttributeString("name", "Z");
-            xw.WriteAttributeString("type", "float");
-            xw.WriteEndElement();
-            xw.WriteEndElement();// End Accessor
-            xw.WriteEndElement();// End technique common
-            xw.WriteEndElement();// End source
-
-            //
-            // UV
-            //
-
-            for (int i = 0; i < geometry.Count; i++)
-            {
-                Single[] uvArray = GetUVArray(i);
-
-                //if (index[i].zone == Zone.Render)
-                {
-                    xw.WriteStartElement("source");
-                    xw.WriteAttributeString("id", modelID + "-uv-" + i.ToString());
-                    xw.WriteStartElement("float_array");
-                    xw.WriteAttributeString("id", modelID + "-uv-array");
-                    xw.WriteAttributeString("count", uvArray.Length.ToString());
-                    xw.WriteString(FileTools.ArrayToStringGeneric<Single>(uvArray, " "));
-                    xw.WriteEndElement();
-                    xw.WriteStartElement("technique_common");
-                    xw.WriteStartElement("accessor");
-                    xw.WriteAttributeString("stride", "2");
-                    xw.WriteAttributeString("source", "#" + modelID + "-uv-array");
-                    xw.WriteAttributeString("count", (uvArray.Length / 2).ToString());
-                    xw.WriteStartElement("param");
-                    xw.WriteAttributeString("name", "S");
-                    xw.WriteAttributeString("type", "float");
-                    xw.WriteEndElement();
-                    xw.WriteStartElement("param");
-                    xw.WriteAttributeString("name", "T");
-                    xw.WriteAttributeString("type", "float");
-                    xw.WriteEndElement();
-                    xw.WriteEndElement();// End Accessor
-                    xw.WriteEndElement();// End technique common
-                    xw.WriteEndElement();// End source
-                }
-            }
-
-            xw.WriteStartElement("vertices");
-            xw.WriteAttributeString("id", modelID + "-vertices");
-            xw.WriteStartElement("input");
-            xw.WriteAttributeString("semantic", "POSITION");
-            xw.WriteAttributeString("source", "#" + modelID + "-positions");
-            xw.WriteEndElement();
-            xw.WriteEndElement();
-
-            //
-            // INDEX:
-            //
-
-            for (int i = 0; i < index.Count; i++)
-            {
-                UInt16[] triangleArray = GetTriangleArray(i);
-
-                xw.WriteStartElement("triangles");
-                xw.WriteAttributeString("count", (triangleArray.Length / 3).ToString());
-                xw.WriteAttributeString("material", "initialShadingGroup");
-                xw.WriteStartElement("input");
-                xw.WriteAttributeString("offset", "0");
-                xw.WriteAttributeString("semantic", "VERTEX");
-                xw.WriteAttributeString("source", "#" + modelID + "-vertices");
-                xw.WriteEndElement();
-                xw.WriteStartElement("input");
-                xw.WriteAttributeString("offset", "0");
-                xw.WriteAttributeString("semantic", "TEXCOORD");
-                xw.WriteAttributeString("source", "#" + modelID + "-uv-" + i.ToString());
-                xw.WriteEndElement();
-                xw.WriteStartElement("p");
-                xw.WriteString(FileTools.ArrayToStringGeneric<UInt16>(triangleArray, " "));
-                xw.WriteEndElement();
-                xw.WriteEndElement();
-            }
-
-            xw.WriteEndElement();// End mesh
-            xw.WriteEndElement();// End geometry
-            xw.WriteEndElement();// End library_geometries
-
-            //
-            // VISUAL SCENE:
-            //
-
-            xw.WriteStartElement("library_visual_scenes");
-            xw.WriteStartElement("visual_scene");
-            xw.WriteAttributeString("id", "scene");
-            xw.WriteStartElement("node");
-            xw.WriteAttributeString("id", modelID + "-geometry");
-            xw.WriteStartElement("rotate");
-            xw.WriteAttributeString("sid", "rotateZ");
-            xw.WriteString("0 0 1 0");
-            xw.WriteEndElement();
-            xw.WriteStartElement("rotate");
-            xw.WriteAttributeString("sid", "rotateY");
-            xw.WriteString("0 1 0 0");
-            xw.WriteEndElement();
-            xw.WriteStartElement("rotate");
-            xw.WriteAttributeString("sid", "rotateX");
-            xw.WriteString("1 0 0 0");
-            xw.WriteEndElement();
-            xw.WriteStartElement("instance_geometry");
-            xw.WriteAttributeString("url", "#" + modelID);
-            xw.WriteStartElement("bind_material");
-            xw.WriteStartElement("technique_common");
-            xw.WriteStartElement("instance_material");
-            xw.WriteAttributeString("target", "#default");
-            xw.WriteAttributeString("symbol", "initialShadingGroup");
-            xw.WriteEndElement(); // instance_material
-            xw.WriteEndElement(); // technique_common
-            xw.WriteEndElement(); // bind_material
-            xw.WriteEndElement(); // instance_geometry
-            xw.WriteEndElement(); // node
-            xw.WriteEndElement(); // visual scene
-            xw.WriteEndElement(); // library visual scenes
-
-            xw.WriteStartElement("scene");
-            xw.WriteStartElement("instance_visual_scene");
-            xw.WriteAttributeString("url", "#scene");
-            xw.WriteEndElement();
-            xw.WriteEndElement();
-
-            xw.WriteEndElement();// End COLLADA
-            xw.WriteEndDocument();
-            xw.Flush();
-
-            FileStream stream = new FileStream(@"D:\\out.dae", FileMode.OpenOrCreate);
-            stream.Write(ms.ToArray(), 0, ms.ToArray().Length);
-            stream.Close();
-
-        }
-
-        //
         // PRIMARY STRUCTURES:
         //
 
-        class Index
+        public class Index
         {
             public Zone zone { get; set; }
             public UInt32 unknown02 { get; set; }
@@ -548,7 +218,7 @@ namespace Reanimator
             public Coordinate[] dimension { get; set; }
         }
 
-        class Geometry
+        public class Geometry
         {
             public UInt32 unknown01 { get; set; }
             public UInt32 coordinates { get; set; }
@@ -575,28 +245,28 @@ namespace Reanimator
         // ENUMERATIONS:
         //
 
-        enum Type : uint
+        public enum Type : uint
         {
             Unit = 0x1B0061E1,
             Background = 0xCAFE1515,
             Player = 0x43342ABF
         }
 
-        enum Detail : uint
+        public enum Detail : uint
         {
             Simple = 0x03,
             Extended = 0x01,
             Full = 0x04
         }
 
-        enum Zone : uint
+        public enum Zone : uint
         {
             Collision = 0x0026,
             Origin = 0x0020,
             Render = 0x0420
         }
 
-        enum Structure : uint
+        public enum Structure : uint
         {
             UnitIndex = 0x02,
             PlayerIndex = 0x08,
@@ -606,7 +276,7 @@ namespace Reanimator
             Footer = 0x06
         }
 
-        enum Shape : uint
+        public enum Shape : uint
         {
             Triangle = 0x03,
             Square = 0x04
@@ -617,7 +287,7 @@ namespace Reanimator
         //
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        class Table
+        public class Table
         {
             public UInt32 id { get; set; }
             public UInt32 offset { get; set; }
@@ -625,7 +295,7 @@ namespace Reanimator
         }
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        class Triangle
+        public class Triangle
         {
             public UInt16 coordinate01 { get; set; }
             public UInt16 coordinate02 { get; set; }
@@ -633,15 +303,20 @@ namespace Reanimator
         }
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        class Coordinate
+        public class Coordinate
         {
             public Single positionX { get; set; }
             public Single positionY { get; set; }
             public Single positionZ { get; set; }
+
+            new public String ToString()
+            {
+                return positionX.ToString() + " " + positionY.ToString() + " " + positionZ.ToString();
+            }
         }
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        class Extended : Coordinate
+        public class Extended : Coordinate
         {
             public Single tanX { get; set; }
             public Single tanY { get; set; }
@@ -650,10 +325,15 @@ namespace Reanimator
         }
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        class UV
+        public class UV
         {
             public Single s { get; set; }
             public Single t { get; set; }
+
+            new public string ToString()
+            {
+                return s.ToString() + " " + t.ToString();
+            }
         }
     }
 }
