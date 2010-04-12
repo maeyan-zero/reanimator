05c5ae2adee56c9f0f53567c8edf7ed88baa61be
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-04-12 13:26:41 +0000

    Minor modification interface improvements

--- trunk/source/Forms/ModificationForm.Designer.cs
+++ trunk/source/Forms/ModificationForm.Designer.cs
@@ -39,7 +39,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.button1 = new System.Windows.Forms.Button();
             this.titleLabel = new System.Windows.Forms.Label();
@@ -65,7 +64,7 @@
             this.checkedListBox.Location = new System.Drawing.Point(6, 19);
             this.checkedListBox.Name = "checkedListBox";
             this.checkedListBox.Size = new System.Drawing.Size(437, 124);
-            this.checkedListBox.TabIndex = 3;
+            this.checkedListBox.TabIndex = 1;
             this.checkedListBox.SelectedIndexChanged += new System.EventHandler(this.checkedListBox_SelectedIndexChanged);
             this.checkedListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox_ItemCheck);
             // 
@@ -118,16 +117,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(7, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.Size = new System.Drawing.Size(35, 17);
             this.label1.TabIndex = 11;
             this.label1.Text = "Title";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 25);
+            this.label2.Location = new System.Drawing.Point(8, 27);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 13);
             this.label2.TabIndex = 12;
@@ -136,7 +136,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 42);
+            this.label3.Location = new System.Drawing.Point(8, 44);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 13;
@@ -145,7 +145,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 59);
+            this.label4.Location = new System.Drawing.Point(8, 61);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(20, 13);
             this.label4.TabIndex = 14;
@@ -154,23 +154,15 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(8, 75);
+            this.label5.Location = new System.Drawing.Point(8, 77);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(31, 13);
             this.label5.TabIndex = 15;
             this.label5.Text = "Type";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(8, 92);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 13);
-            this.label6.TabIndex = 16;
-            this.label6.Text = "Description";
-            // 
             // splitContainer1
             // 
+            this.splitContainer1.IsSplitterFixed = true;
             this.splitContainer1.Location = new System.Drawing.Point(6, 19);
             this.splitContainer1.Name = "splitContainer1";
             // 
@@ -187,7 +179,6 @@
             this.splitContainer1.Panel2.Controls.Add(this.versionLabel);
             this.splitContainer1.Panel2.Controls.Add(this.titleLabel);
             this.splitContainer1.Panel2.Controls.Add(this.label1);
-            this.splitContainer1.Panel2.Controls.Add(this.label6);
             this.splitContainer1.Panel2.Controls.Add(this.label2);
             this.splitContainer1.Panel2.Controls.Add(this.label5);
             this.splitContainer1.Panel2.Controls.Add(this.label3);
@@ -198,7 +189,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(9, 412);
+            this.button1.Location = new System.Drawing.Point(9, 416);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(449, 23);
             this.button1.TabIndex = 13;
@@ -209,16 +200,17 @@
             // titleLabel
             // 
             this.titleLabel.AutoSize = true;
-            this.titleLabel.Location = new System.Drawing.Point(90, 8);
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(88, 8);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(49, 13);
+            this.titleLabel.Size = new System.Drawing.Size(65, 17);
             this.titleLabel.TabIndex = 17;
             this.titleLabel.Text = "titleLabel";
             // 
             // versionLabel
             // 
             this.versionLabel.AutoSize = true;
-            this.versionLabel.Location = new System.Drawing.Point(89, 25);
+            this.versionLabel.Location = new System.Drawing.Point(89, 27);
             this.versionLabel.Name = "versionLabel";
             this.versionLabel.Size = new System.Drawing.Size(67, 13);
             this.versionLabel.TabIndex = 18;
@@ -227,7 +219,7 @@
             // authorLabel
             // 
             this.authorLabel.AutoSize = true;
-            this.authorLabel.Location = new System.Drawing.Point(89, 42);
+            this.authorLabel.Location = new System.Drawing.Point(89, 44);
             this.authorLabel.Name = "authorLabel";
             this.authorLabel.Size = new System.Drawing.Size(63, 13);
             this.authorLabel.TabIndex = 19;
@@ -236,7 +228,7 @@
             // urlLabel
             // 
             this.urlLabel.AutoSize = true;
-            this.urlLabel.Location = new System.Drawing.Point(90, 59);
+            this.urlLabel.Location = new System.Drawing.Point(90, 61);
             this.urlLabel.Name = "urlLabel";
             this.urlLabel.Size = new System.Drawing.Size(44, 13);
             this.urlLabel.TabIndex = 20;
@@ -245,7 +237,7 @@
             // typeLabel
             // 
             this.typeLabel.AutoSize = true;
-            this.typeLabel.Location = new System.Drawing.Point(90, 75);
+            this.typeLabel.Location = new System.Drawing.Point(90, 77);
             this.typeLabel.Name = "typeLabel";
             this.typeLabel.Size = new System.Drawing.Size(53, 13);
             this.typeLabel.TabIndex = 21;
@@ -255,18 +247,18 @@
             // 
             this.descriptionText.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.descriptionText.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.descriptionText.Location = new System.Drawing.Point(11, 108);
+            this.descriptionText.Location = new System.Drawing.Point(11, 105);
             this.descriptionText.Multiline = true;
             this.descriptionText.Name = "descriptionText";
             this.descriptionText.ReadOnly = true;
-            this.descriptionText.Size = new System.Drawing.Size(259, 73);
+            this.descriptionText.Size = new System.Drawing.Size(259, 72);
             this.descriptionText.TabIndex = 22;
             // 
             // ModificationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 449);
+            this.ClientSize = new System.Drawing.Size(468, 452);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -303,7 +295,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Label authorLabel;
         private System.Windows.Forms.Label versionLabel;
