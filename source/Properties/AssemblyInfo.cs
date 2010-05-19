3d6b20736a50c26150e84e6fc53d2d2ee4a9fb4d
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-05-19 21:41:15 +0000

    Changed assembly info and other aesthetics,

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Reanimator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The ultimate Hellgate London modification toolset")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Revival Team")]
 [assembly: AssemblyProduct("Reanimator")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Revival Team 2009-2010")]
+[assembly: AssemblyTrademark("http://www.hellgateaus.net")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
