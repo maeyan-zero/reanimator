322df572185a778f7abd58721523d9fdf0a51bd0
alex.waters <alex.waters@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-08-19 10:32:19 +0000

    Starting preparations for XmlCooked serialization.

---
+++ trunk/source/XmlCookedFile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Reanimator
+{
+    class XmlCookedFile
+    {
+        private XmlCookedBase _xmlCooked;
+
+        public XmlCookedFile()
+        {
+            _xmlCooked = null;
+        }
+
+        public bool ParseData(byte[] data)
+        {
+            if (data == null) return false;
+
+            const int offset = 8;
+            if (offset >= data.Length - 4) return false;
+
+            // this isn't actually (at least, I don't think it is) a type specifier, but seeing
+            // as (from what I've seen so far) the unknown array is constant per-type, might as well use it as such
+            uint cookedType = FileTools.ByteArrayTo<uint>(data, offset);
+
+            switch(cookedType)
+            {
+                case 0x400053E7: // skills
+                    _xmlCooked = new XmlCookedSkill();
+                    break;
+
+                case 0x3A048D4A: // states
+                    _xmlCooked = new XmlCookedState();
+                    break;
+
+                default:
+                    MessageBox.Show("Not implemented type!\nOnly skills and states supported.\n\nunknown = 0x" + cookedType.ToString("X8"), "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    Debug.Assert(false);
+                    return false;
+            }
+
+            return _xmlCooked.ParseData(data);
+        }
+
+        public void SaveXml(String path)
+        {
+            if (_xmlCooked.XmlDoc == null || String.IsNullOrEmpty(path)) return;
+
+            _xmlCooked.XmlDoc.Save(path);
+        }
+    }
+}
---
+++ trunk/source/XmlCookedState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml;
+
+namespace Reanimator
+{
+    class XmlCookedState : XmlCookedBase
+    {
+        protected override bool ParseDataSegment(XmlElement dataElement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
