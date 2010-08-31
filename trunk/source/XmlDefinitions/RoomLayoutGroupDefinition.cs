292c7a3d891b75cf880e3d572081afcf5b734785
alex.waters <alex.waters@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-08-31 08:53:18 +0000

    Forgot XML Definitions.

---
+++ trunk/source/XmlDefinitions/RoomLayoutGroupDefinition.cs
@@ -0,0 +1,46 @@
+﻿namespace Reanimator.XmlDefinitions
+{
+    class RoomLayoutGroupDefinition : XmlDefinition
+    {
+        private new static readonly XmlCookElement[] Elements =
+        {
+            new XmlCookElement
+            {
+                Name = "tGroup",
+                ElementType = ElementType.Table,
+                DefaultValue = null,
+                ChildType = typeof (RoomLayoutGroup)
+            },
+            new XmlCookElement
+            {
+                Name = "bShowIcons",
+                ElementType = ElementType.Int32,
+                DefaultValue = 0, // FALSE
+            },
+            new XmlCookElement
+            {
+                Name = "nEditType",
+                ElementType = ElementType.Int32,
+                DefaultValue = 0, // EDIT_TYPE_POLY_PICK // todo: fix me?
+            },
+            new XmlCookElement
+            {
+                Name = "bExists",
+                ElementType = ElementType.Int32,
+                DefaultValue = 0, // FALSE
+            },
+            new XmlCookElement
+            {
+                Name = "bFixup",
+                ElementType = ElementType.Int32,
+                DefaultValue = 0, // FALSE
+            },
+        };
+
+        public RoomLayoutGroupDefinition()
+        {
+            RootElement = "ROOM_LAYOUT_GROUP_DEFINITION";
+            base.Elements.AddRange(Elements);
+        }
+    }
+}
