cccb488033e558d4d869e26945a2cf71b05e9746
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-11-05 02:56:16 +0000

    Major revision of the Index file. Other files that have changed are mainly due to the refactoring

--- hellgate/ExcelFile.cs
+++ hellgate/ExcelFile.cs
@@ -1227,7 +1227,7 @@ namespace Hellgate
 
                 if (dc.ExtendedProperties.Contains(ColumnTypeKeys.SortDistinctID))
                 {
-                    sortTable = SelectDistinct(sortTable, new string[] { sortColumn });
+                    sortTable = TableTools.SelectDistinct(sortTable, new string[] { sortColumn });
                     dataView = sortTable.DefaultView;
                 }
 
@@ -1420,72 +1420,6 @@ namespace Hellgate
                 return attribute as OutputAttribute;
             }
             return null;
-        }
-
-        private static DataTable SelectDistinct(DataTable SourceTable, params string[] FieldNames)
-        {
-            object[] lastValues;
-            DataTable newTable;
-            DataRow[] orderedRows;
-
-            if (FieldNames == null || FieldNames.Length == 0)
-                throw new ArgumentNullException("FieldNames");
-
-            lastValues = new object[FieldNames.Length];
-            newTable = new DataTable();
-
-            foreach (string fieldName in FieldNames)
-                newTable.Columns.Add(fieldName, SourceTable.Columns[fieldName].DataType);
-
-            orderedRows = SourceTable.Select("", string.Join(", ", FieldNames));
-
-            foreach (DataRow row in orderedRows)
-            {
-                if ((int)row[FieldNames[0]] == -1) //lazy. function cant be reused
-                {
-                    continue; // ignore values -1
-                }
-
-                if (!fieldValuesAreEqual(lastValues, row, FieldNames))
-                {
-                    DataRow newRow = createRowClone(row, newTable.NewRow(), FieldNames);
-                    newTable.Rows.Add(newRow);
-                    setLastValues(lastValues, row, FieldNames);
-                    newRow[0] = row[0];//modified to store index, not distinct value
-                }
-            }
-
-            return newTable;
-        }
-
-        private static bool fieldValuesAreEqual(object[] lastValues, DataRow currentRow, string[] fieldNames)
-        {
-            bool areEqual = true;
-
-            for (int i = 0; i < fieldNames.Length; i++)
-            {
-                if (lastValues[i] == null || !lastValues[i].Equals(currentRow[fieldNames[i]]))
-                {
-                    areEqual = false;
-                    break;
-                }
-            }
-
-            return areEqual;
-        }
-
-        private static DataRow createRowClone(DataRow sourceRow, DataRow newRow, string[] fieldNames)
-        {
-            foreach (string field in fieldNames)
-                newRow[field] = sourceRow[field];
-
-            return newRow;
-        }
-
-        private static void setLastValues(object[] lastValues, DataRow sourceRow, string[] fieldNames)
-        {
-            for (int i = 0; i < fieldNames.Length; i++)
-                lastValues[i] = sourceRow[fieldNames[i]];
         }
     }
 }
