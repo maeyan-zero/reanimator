a59a3edfcc301070b93ebef0732a162fe21aa902
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-11-11 10:51:09 +0000

    Missing common files committed

---
+++ trunk/common/TableTools.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+
+namespace Revival.Common
+{
+    public static class TableTools
+    {
+        public static DataTable SelectDistinct(DataTable SourceTable, params string[] FieldNames)
+        {
+            object[] lastValues;
+            DataTable newTable;
+            DataRow[] orderedRows;
+
+            if (FieldNames == null || FieldNames.Length == 0)
+                throw new ArgumentNullException("FieldNames");
+
+            lastValues = new object[FieldNames.Length];
+            newTable = new DataTable();
+
+            foreach (string fieldName in FieldNames)
+                newTable.Columns.Add(fieldName, SourceTable.Columns[fieldName].DataType);
+
+            orderedRows = SourceTable.Select("", string.Join(", ", FieldNames));
+
+            foreach (DataRow row in orderedRows)
+            {
+                if ((int)row[FieldNames[0]] == -1) //lazy. function cant be reused
+                {
+                    continue; // ignore values -1
+                }
+
+                if (!FieldValuesAreEqual(lastValues, row, FieldNames))
+                {
+                    DataRow newRow = CreateRowClone(row, newTable.NewRow(), FieldNames);
+                    newTable.Rows.Add(newRow);
+                    SetLastValues(lastValues, row, FieldNames);
+                    newRow[0] = row[0];//modified to store index, not distinct value
+                }
+            }
+
+            return newTable;
+        }
+
+        public static bool FieldValuesAreEqual(object[] lastValues, DataRow currentRow, string[] fieldNames)
+        {
+            bool areEqual = true;
+
+            for (int i = 0; i < fieldNames.Length; i++)
+            {
+                if (lastValues[i] == null || !lastValues[i].Equals(currentRow[fieldNames[i]]))
+                {
+                    areEqual = false;
+                    break;
+                }
+            }
+
+            return areEqual;
+        }
+
+        public static DataRow CreateRowClone(DataRow sourceRow, DataRow newRow, string[] fieldNames)
+        {
+            foreach (string field in fieldNames)
+                newRow[field] = sourceRow[field];
+
+            return newRow;
+        }
+
+        private static void SetLastValues(object[] lastValues, DataRow sourceRow, string[] fieldNames)
+        {
+            for (int i = 0; i < fieldNames.Length; i++)
+                lastValues[i] = sourceRow[fieldNames[i]];
+        }
+    }
+}
---
+++ trunk/common/XmlUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+using System.IO;
+
+namespace Revival.Common
+{
+    public static class XmlUtilities<T>
+    {
+        public static void Serialize(T type, string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            TextWriter tw = new StreamWriter(path);
+            serializer.Serialize(tw, type);
+            tw.Close();
+        }
+
+        public static T Deserialize(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            TextReader tr = new StreamReader(path);
+            T type = (T)serializer.Deserialize(tr);
+            tr.Close();
+
+            return type;
+        }
+    }
+}
