00d29fa519e97d20e514598b250ba99c38557313
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-11-11 10:51:09 +0000

    Missing common files committed

---
+++ common/Exceptions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Revival.Common
+{
+    static class Exceptions
+    {
+        internal class ReanimatorException : Exception
+        {
+            protected String CustomMessage;
+
+            public override string ToString()
+            {
+                return String.Format("{0}\n{1}", CustomMessage, StackTrace);
+            }
+        }
+
+        public class UnexpectedMagicWordException : ReanimatorException
+        {
+            public UnexpectedMagicWordException()
+            {
+                CustomMessage = "Unexpected file header Magic Word!";
+            }
+        }
+
+        public class NotSupportedFileVersionException : ReanimatorException
+        {
+            public NotSupportedFileVersionException()
+            {
+                CustomMessage = "Unexpected file header version!";
+            }
+        }
+
+        public class NotInitializedException : ReanimatorException
+        {
+            public NotInitializedException()
+            {
+                CustomMessage = "Class Not Initialized!";
+            }
+        }
+
+        public class NotSupportedFileDefinitionException : ReanimatorException
+        {
+            public NotSupportedFileDefinitionException()
+            {
+                CustomMessage = "File definition type not supported!";
+            }
+        }
+
+        public class UnexpectedTokenException : ReanimatorException
+        {
+            public UnexpectedTokenException(String str)
+            {
+                CustomMessage = str;
+            }
+        }
+
+        public class UnknownExcelElementException : ReanimatorException
+        {
+            public UnknownExcelElementException(String excelElement)
+            {
+                if (String.IsNullOrEmpty(excelElement)) excelElement = String.Empty;
+
+                CustomMessage = "An unknown Excel Element was encountered!\n" + excelElement;
+            }
+        }
+    }
+}
