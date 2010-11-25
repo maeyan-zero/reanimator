b696c550ffe7587b6732f4195af9efb88e9d453a
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-11-25 01:20:53 +0000

    + Appended missing sort attribute to WardrobeBody.cs. The Entire mod should build correctly now

--- hellgate/Excel/WardrobeBody.cs
+++ hellgate/Excel/WardrobeBody.cs
@@ -9,6 +9,7 @@ namespace Hellgate.Excel
     class WardrobeBody
     {
         TableHeader header;
+        [ExcelOutput(SortAscendingID = 1)]
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
         public string name;
         public Int32 layers1;
