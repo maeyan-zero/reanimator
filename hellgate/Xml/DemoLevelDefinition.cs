5d82d05484df53b86fa45d3689f4f11e64fdac78
alex.waters <alex.waters@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-11-30 13:03:25 +0000

    Opps, forgot file.

---
+++ hellgate/Xml/DemoLevelDefinition.cs
@@ -0,0 +1,53 @@
+﻿namespace Hellgate.Xml
+{
+    class DemoLevelDefinition : XmlDefinition
+    {
+        private new static readonly XmlCookElement[] Elements =
+        {
+            new XmlCookElement
+            {
+                Name = "dwFlags",
+                DefaultValue = 0,
+                ElementType = ElementType.Int32
+            },
+            new XmlCookElement
+            {
+                Name = "nLevelDefinition",
+                DefaultValue = null,
+                ElementType = ElementType.ExcelIndex,
+                ExcelTableCode = 29233 // LEVEL
+            },
+            new XmlCookElement
+            {
+                Name = "nDRLGDefinition",
+                DefaultValue = null,
+                ElementType = ElementType.ExcelIndex,
+                ExcelTableCode = 21553 // LEVEL_DRLGS
+            },
+            new XmlCookElement
+            {
+                Name = "dwDRLGSeed",
+                DefaultValue = 1,
+                ElementType = ElementType.Int32
+            },
+            new XmlCookElement
+            {
+                Name = "nCameraType",
+                DefaultValue = 0,
+                ElementType = ElementType.Int32
+            },
+            new XmlCookElement
+            {
+                Name = "fMetersPerSecond",
+                DefaultValue = 5.0f,
+                ElementType = ElementType.Float
+            }
+        };
+
+        public DemoLevelDefinition()
+        {
+            RootElement = "DEMO_LEVEL_DEFINITION";
+            base.Elements.AddRange(Elements);
+        }
+    }
+}
