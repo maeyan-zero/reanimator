0be6c09c9a315d38536a588a0edc9c475b264873
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-12-04 09:03:34 +0000

    + Mostly work related to the Launcher. Nothing specific to report, as all parts were developed

--- hellgate/Common.cs
+++ hellgate/Common.cs
@@ -2,10 +2,29 @@
 {
     public static class Common
     {
+        public static string SaveLaunchCommand = "{0}\\Launcher.exe -load\"{1}\""; // 0 = hgldir, 1 = save file(?)
+        public static string InstallPathRegLocation = @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Flagship Studios\Hellgate London";
         public static string DefaultHellgatePath = @"C:\Program Files\Flagship Studios\Hellgate London";
-        public static string DataPath = "data\\";
-        public static string DataCommonPath = "data_common\\";
+        public static string DataPath = @"data\";
+        public static string DataCommonPath = @"data_common\";
         public static string[] MPFiles = new string[] { "hellgate*", "mp_hellgate*" };
         public static string[] SPFiles = new string[] { "hellgate*", "sp_hellgate*" };
+        public static string[] OriginalDats = new string[]
+        {
+            "hellgate000",
+            "hellgate_soundmusic000",
+            "hellgate_sound000",
+            "hellgate_playershigh000",
+            "hellgate_movieslow000",
+            "hellgate_movieshigh000",
+            "hellgate_movies000",
+            "hellgate_localized000",
+            "hellgate_graphicshigh000",
+            "hellgate_bghigh000",
+            "mp_hellgate_1.10.180.3416_1.0.86.4580",
+            "mp_hellgate_localized_1.10.180.3416_1.0.86.4580",
+            "sp_hellgate_1.10.180.3416_1.18074.70.4256",
+            "sp_hellgate_localized_1.10.180.3416_1.18074.70.4256"
+        };
     }
 }
--- launcher/Forms/OptionsForm.cs
+++ launcher/Forms/OptionsForm.cs
@@ -9,11 +9,64 @@ using System.Windows.Forms;
 
 namespace Launcher.Forms
 {
-    public partial class Options : Form
+    public partial class OptionsForm : Form
     {
-        public Options()
+        public OptionsForm()
         {
             InitializeComponent();
+        }
+
+        private void OptionsForm_Load(object sender, EventArgs e)
+        {
+            hellgateTextBox.Text = Config.HglDir;
+            clientTextBox.Text = Config.GameClientPath;
+            saveTextBox.Text = Config.SaveDir;
+            backupTextBox.Text = Config.BackupDir;
+        }
+
+        private void PathBrowseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrower = new FolderBrowserDialog();
+            if (folderBrower.ShowDialog() == DialogResult.OK)
+            {
+                if ((Button)sender == hellgateButton)
+                {
+                    hellgateTextBox.Text = folderBrower.SelectedPath;
+                    return;
+                }
+                if ((Button)sender == saveButton)
+                {
+                    saveTextBox.Text = folderBrower.SelectedPath;
+                    return;
+                }
+                if ((Button)sender == backupButton)
+                {
+                    backupTextBox.Text = folderBrower.SelectedPath;
+                    return;
+                }
+            }
+        }
+
+        private void FileBrowseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((Button)sender == clientButton)
+                {
+                    clientTextBox.Text = fileDialog.FileName;
+                    return;
+                }
+            }
+        }
+
+        private void AcceptButton_Click(object sender, EventArgs e)
+        {
+            Config.HglDir = hellgateTextBox.Text;
+            Config.GameClientPath = clientTextBox.Text;
+            Config.SaveDir = saveTextBox.Text;
+            Config.BackupDir = backupTextBox.Text;
+            this.Close();
         }
     }
 }
