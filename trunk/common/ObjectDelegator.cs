c5365f263e0c67810a14adb90a74aead737027c9
alex.waters <alex.waters@0aafadf6-0629-11df-8138-214a9d281a8c>
2010-12-17 13:06:18 +0000

    Massive boost to DataTable generation. (similar to last commit) Fixed bug where some tables weren't displaying relations when generated from another table.

--- trunk/common/ObjectDelegator.cs
+++ trunk/common/ObjectDelegator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 
@@ -7,6 +8,7 @@ namespace Revival.Common
     public class ObjectDelegator
     {
         public delegate Object FieldGetValueDelegate(Object target);
+        private readonly Dictionary<String, FieldGetValueDelegate> _fieldGetValueDelegatesDict;
         private readonly FieldGetValueDelegate[] _fieldGetValueDelegates;
 
         private delegate void FieldSetValueDelegate(Object target, Object value);
@@ -21,7 +23,8 @@ namespace Revival.Common
                     _fieldGetValueDelegates = new FieldGetValueDelegate[fieldInfos.Length];
                     for (int i = 0; i < fieldInfos.Length; i++)
                     {
-                        _fieldGetValueDelegates[i] = _CreateGetField(fieldInfos[i]);
+                        FieldGetValueDelegate getValueDelegate = _CreateGetField(fieldInfos[i]);
+                        _fieldGetValueDelegates[i] = getValueDelegate;
                     }
                     break;
 
@@ -32,6 +35,32 @@ namespace Revival.Common
                         _fieldSetValueDelegates[i] = _CreateSetField(fieldInfos[i]);
                     }
                     break;
+
+                default:
+                    throw new NotSupportedException("The field " + field + "is not supported!");
+            }
+        }
+
+        // todo: really should change the CSV stuff to have it use like objectDelegate["field"] like the DataTables and remove above ctor()
+        public ObjectDelegator(IList<FieldInfo> fieldInfos, String field)
+        {
+            switch (field)
+            {
+                case "GetValue":
+                    _fieldGetValueDelegatesDict = new Dictionary<String, FieldGetValueDelegate>();
+                    foreach (FieldInfo fieldInfo in fieldInfos)
+                    {
+                        _fieldGetValueDelegatesDict.Add(fieldInfo.Name, _CreateGetField(fieldInfo));
+                    }
+                    break;
+
+                //case "SetValue":
+                //    _fieldSetValueDelegatesDict = new FieldSetValueDelegate[fieldInfos.Length];
+                //    for (int i = 0; i < fieldInfos.Length; i++)
+                //    {
+                //        _fieldSetValueDelegates[i] = _CreateSetField(fieldInfos[i]);
+                //    }
+                //    break;
 
                 default:
                     throw new NotSupportedException("The field " + field + "is not supported!");
@@ -88,6 +117,14 @@ namespace Revival.Common
                 if (index < 0 || index > _fieldGetValueDelegates.Length) return null;
 
                 return _fieldGetValueDelegates[index];
+            }
+        }
+
+        public FieldGetValueDelegate this[String index]
+        {
+            get
+            {
+                return _fieldGetValueDelegatesDict[index];
             }
         }
 
