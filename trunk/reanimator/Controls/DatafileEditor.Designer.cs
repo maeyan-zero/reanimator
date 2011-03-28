38c3a2a00d50051bfd64c308c7c64b5e1ccab7d8
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-03-28 12:38:42 +0000

    + New controls required for Excel Editor

---
+++ trunk/reanimator/Controls/DatafileEditor.Designer.cs
@@ -0,0 +1,40 @@
+﻿namespace Reanimator.Controls
+{
+    partial class DatafileEditor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void _InitializeComponent()
+        {
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView _tableData_DataGridView;
+        private System.Windows.Forms.TableLayoutPanel rows_LayoutPanel;
+    }
+}
