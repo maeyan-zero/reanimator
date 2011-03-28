40dd8a13d3d067db71fbc9c9b484ad79a19ac29c
maeyan.zero <maeyan.zero@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-03-28 18:36:15 +0000

    Mostly refactoring and commenting

--- reanimator/Controls/TablesLoaded.cs
+++ reanimator/Controls/TablesLoaded.cs
@@ -16,23 +16,33 @@ namespace Reanimator.Controls
         private readonly FileManager _fileManager;
         private TableEditorForm _excelTableForm;
 
+        /// <summary>
+        /// Constructs a list of tables that can be clicked to view.
+        /// </summary>
+        /// <param name="fileManager">FileManager dependency.</param>
+        /// <param name="excelTableForm">Parents control of tables..</param>
         public TablesLoaded(FileManager fileManager, TableEditorForm excelTableForm)
         {
-            InitializeComponent();
+            _InitializeComponent();
 
             _fileManager = fileManager;
             _excelTableForm = excelTableForm;
 
             if (_fileManager == null || _fileManager.DataFiles.Count == 0) return;
-            loadedTables_ListBox.DataSource = new BindingSource(_fileManager.DataFiles, null);
-            loadedTables_ListBox.DoubleClick += new EventHandler(_ListBox1_MouseDoubleClick);
-            loadedTables_ListBox.Format += new ListControlConvertEventHandler(_LoadedTables_ListBox_Format);
+            _loadedTables_ListBox.DataSource = new BindingSource(_fileManager.DataFiles, null);
+            _loadedTables_ListBox.DoubleClick += new EventHandler(_loadedTables_ListBox_MouseDoubleClick);
+            _loadedTables_ListBox.Format += new ListControlConvertEventHandler(_loadedTables_ListBox_Format);
         }
 
-        private void _ListBox1_MouseDoubleClick(object sender, EventArgs e)
+        /// <summary>
+        /// Creates a mew tab control for the clicked table.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _loadedTables_ListBox_MouseDoubleClick(object sender, EventArgs e)
         {
             KeyValuePair<String, DataFile> item =
-                    (KeyValuePair<String, DataFile>)loadedTables_ListBox.SelectedItem;
+                    (KeyValuePair<String, DataFile>)_loadedTables_ListBox.SelectedItem;
 
             if (_excelTableForm == null || _excelTableForm.IsDisposed)
             {
@@ -54,7 +64,12 @@ namespace Reanimator.Controls
         }
 
 
-        private void _LoadedTables_ListBox_Format(object sender, ListControlConvertEventArgs e)
+        /// <summary>
+        /// Defines the list format.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _loadedTables_ListBox_Format(object sender, ListControlConvertEventArgs e)
         {
             KeyValuePair<String, DataFile> item = (KeyValuePair<String, DataFile>)e.ListItem;
             e.Value = item.Key;
