ab3d88ac3a1335473ab9d28ac49b1e8aaceed839
KiteDX <KiteDX@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-03-29 13:15:31 +0000

    Added missing files

---
+++ trunk/reanimator/Forms/DropOverviewForm/DropOverview.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Hellgate;
+using System.Diagnostics;
+
+namespace Reanimator.Forms.DropOverviewForm
+{
+    public partial class DropOverview : Form
+    {
+        const int NAMECOLUMNINDEX = 2;
+        FileManager _fileManager;
+        //TableDataSet _dataSet;
+        //FileExplorer _explorer;
+        DataTable _treasureTable;
+        DataTable _itemsTable;
+        DataTable _unitTypesTable;
+        DataTable _itemQualityTable;
+
+        public DropOverview(FileManager fileManager)
+        {
+            InitializeComponent();
+
+            _fileManager = fileManager;
+
+            _treasureTable = _fileManager.GetDataTable("TREASURE");
+            _itemsTable = _fileManager.GetDataTable("ITEMS");
+            _unitTypesTable = _fileManager.GetDataTable("UNITTYPES");
+            _itemQualityTable = _fileManager.GetDataTable("ITEM_QUALITY");
+
+            for (int counter = 0; counter < _treasureTable.Rows.Count; counter++)
+            {
+                DataRow row = _treasureTable.Rows[counter];
+                string name = (string)row[NAMECOLUMNINDEX];
+
+                if(name != "")
+                {
+                    cb_dropTables.Items.Add(name);
+                    cb_dropTables.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private void cb_dropTables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow row = GetRowFromName(_treasureTable, cb_dropTables.Text);
+                TreasureTableEntry entry;
+
+                if (row != null)
+                {
+                    TreeNode node = new TreeNode(cb_dropTables.Text);
+                    entry = ParseRow(row, 1.0f, node);
+                    tv_drops.Nodes.Clear();
+                    tv_drops.Nodes.Add(node);
+                }
+
+                if (cb_expandTree.Checked)
+                {
+                    tv_drops.ExpandAll();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "SelectedIndexChanged");
+            }
+        }
+
+        private DataRow GetRowFromName(DataTable table, string rowName)
+        {
+            try
+            {
+                for (int counter = 0; counter < table.Rows.Count; counter++)
+                {
+                    DataRow row = table.Rows[counter];
+                    string name = (string)row[NAMECOLUMNINDEX];
+
+                    if (name == rowName)
+                    {
+                        return row;
+                    }
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "GetRowFromName");
+                return null;
+            }
+        }
+
+        private TreasureTableEntry ParseRow(DataRow row, float dropChance, TreeNode node)
+        {
+            int tablePosition = 13;
+            float noDrop = 0;
+            bool picks = false;
+            string drops = "";
+
+            try
+            {
+                bool pickTypes = (int)row["pickType"] != 0;
+                string pickString = (string)row["picks"];
+                picks = !pickString.StartsWith("0");
+
+                //int tablePosition = 30;
+                //the index of the first item entry 13 for SP, 19 for MP
+                noDrop = (float)row["noDrop"];
+                node.Tag = noDrop;
+                drops = string.Format("{0:0.00}", 100 - noDrop) + "%";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ParseRow(1)");
+            }
+            try
+            {
+                TreasureTableEntry tableEntry = new TreasureTableEntry();
+
+ 
+                int numberOfEntries = CountItemEntries(row, picks, tablePosition);
+
+                float generalDropChance = dropChance * (1.0f - noDrop / 100) * (1.0f / numberOfEntries);
+
+                for (int counter = 1; counter <= 8; counter++)
+                {
+                    TreasureTableItemEntry entry = new TreasureTableItemEntry();
+
+                    string entryString = (string)row[tablePosition];
+                    Debug.Assert(entryString != null);
+
+                    string[] entries = entryString.Split(',');
+                    entry.tableToUse = int.Parse(entries[0]);
+                    tablePosition++;
+                    entry.entryId = int.Parse(entries[1]);
+                    entry.quantity = (int)row[tablePosition];
+                    tablePosition++;
+
+                    entry.dropChance = generalDropChance;
+
+                    if (entry.tableToUse == 0)
+                    {
+                        break;
+                    }
+
+                    DataRow referencedRow = null;
+                    if (entry.tableToUse == -1 && entry.entryId > -1)
+                    {
+                        //DataRow refRow = _treasureTable.Rows[entry.entryId];
+                        //entry.name = (string)refRow[1];
+                        //referencedRow = GetRowFromName(_treasureTable, entry.name);
+                    }
+                    else if (entry.tableToUse == 1 && entry.entryId > -1)
+                    {
+                        DataRow refRow = _itemsTable.Rows[entry.entryId];
+                        entry.name = (string)refRow[NAMECOLUMNINDEX];
+                        entry.tableType = "Items";
+                        referencedRow = GetRowFromName(_itemsTable, entry.name);
+                    }
+                    else if (entry.tableToUse == 2 && entry.entryId > -1)
+                    {
+                        DataRow refRow = _unitTypesTable.Rows[entry.entryId];
+                        entry.name = (string)refRow[NAMECOLUMNINDEX];
+                        entry.tableType = "UnitTypes";
+                        referencedRow = GetRowFromName(_unitTypesTable, entry.name);
+                    }
+                    else if (entry.tableToUse == 3 && entry.entryId > -1)
+                    {
+                        DataRow refRow = _treasureTable.Rows[entry.entryId];
+                        entry.name = (string)refRow[NAMECOLUMNINDEX];
+                        entry.tableType = "Treasure";
+                        referencedRow = GetRowFromName(_treasureTable, entry.name);
+                    }
+                    else if (entry.tableToUse == 4 && entry.entryId > -1)
+                    {
+                        DataRow refRow = _itemQualityTable.Rows[entry.entryId];
+                        string name = (string)refRow[NAMECOLUMNINDEX];
+                        entry.name = string.Format("All except \"{0}\"", name);
+                        entry.tableType = "ItemQuality";
+                        referencedRow = GetRowFromName(_itemQualityTable, name);
+                    }
+                    else if (entry.tableToUse == 5 && entry.entryId > -1)
+                    {
+                        //DataRow refRow = _treasureTable.Rows[entry.entryId];
+                        //entry.name = (string)refRow[1];
+                        //referencedRow = GetRowFromName(_treasureTable, entry.name);
+                    }
+
+                    if (referencedRow != null)
+                    {
+                        string chance = string.Format("{0:0.00}", entry.dropChance * 100) + "%";
+
+                        if (entry.name == string.Empty)
+                        {
+                            MessageBox.Show(string.Format("The referenced entry ({0}[{1}]) doesn't have a name! Please check if the reference points to a valid item.", entry.tableType, entry.entryId), "Warning!");
+                        }
+
+                        TreeNode tNode = new TreeNode(entry.tableType + "[" + entry.entryId + "] (" + drops + "): " + entry.name + " - " + chance);
+
+                        if (entry.tableToUse == 3)
+                        {
+                            entry.treasureTableEntry = ParseRow(referencedRow, entry.dropChance, tNode);
+                        }
+
+                        //if only one item will be picked, color the name of the entry red
+                        if (picks == true)
+                        {
+                            tNode.ForeColor = Color.Red;
+                        }
+                        else
+                        {
+                            tNode.ForeColor = Color.Black;
+                        }
+
+                        node.Nodes.Add(tNode);
+                    }
+
+                    if (entry.dropChance * 100 <= 0.05)
+                    {
+                        MessageBox.Show("Warning! The drop rate of the item " + entry.name + " is pretty low (0.05%)! (" + entry.dropChance * 100 + "%)");
+                    }
+
+                    if (entry.tableToUse != 0)
+                    {
+                        tableEntry.itemEntries.Add(entry);
+                    }
+                }
+
+                return tableEntry;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ParseRow(2)");
+                return null;
+            }
+        }
+
+        private int CountItemEntries(DataRow row, bool picks, int tablePosition)
+        {
+            try
+            {
+                int numberOfEntries = 0;
+
+                if (picks)
+                {
+                    //iterate over all 8 item entries
+                    for (int counter = 1; counter <= 8; counter++)
+                    {
+                        //check if the entry is actually used (first number != 0)
+                        string entryString = (string)row[tablePosition + (counter - 1) * 2];
+                        Debug.Assert(entryString != null);
+                        bool used = !entryString.StartsWith("0");
+
+                        if (used)
+                        {
+                            numberOfEntries++;
+                        }
+                    }
+
+                    if (numberOfEntries == 0)
+                    {
+                        MessageBox.Show("Entry doesn't contain any items!", "Warning!");
+                    }
+                }
+                else
+                {
+                    numberOfEntries = 1;
+                }
+                return numberOfEntries;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "CountItemEntries");
+                return 0;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_expandTree.Checked)
+            {
+                tv_drops.ExpandAll();
+            }
+            else
+            {
+                tv_drops.CollapseAll();
+            }
+        }
+    }
+
+
+    public class TreasureTableEntry
+    {
+        public List<TreasureTableItemEntry> itemEntries;
+
+        public TreasureTableEntry()
+        {
+            itemEntries = new List<TreasureTableItemEntry>();
+        }
+    }
+
+    public class TreasureTableItemEntry
+    {
+        public string tableType;
+        public int tableToUse;
+        public int entryId;
+        public int quantity;
+        public float dropChance;
+        public string name;
+        public TreasureTableEntry treasureTableEntry;
+
+        public override string ToString()
+        {
+            return name + " - " + dropChance + "%";
+        }
+    }
+}
---
+++ trunk/reanimator/Forms/DropOverviewForm/DropOverview.designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Reanimator.Forms.DropOverviewForm
+{
+    partial class DropOverview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_dropTables = new System.Windows.Forms.ComboBox();
+            this.tv_drops = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_expandTree = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cb_dropTables
+            // 
+            this.cb_dropTables.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb_dropTables.FormattingEnabled = true;
+            this.cb_dropTables.Location = new System.Drawing.Point(12, 12);
+            this.cb_dropTables.Name = "cb_dropTables";
+            this.cb_dropTables.Size = new System.Drawing.Size(192, 21);
+            this.cb_dropTables.TabIndex = 0;
+            this.cb_dropTables.SelectedIndexChanged += new System.EventHandler(this.cb_dropTables_SelectedIndexChanged);
+            // 
+            // tv_drops
+            // 
+            this.tv_drops.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tv_drops.FullRowSelect = true;
+            this.tv_drops.Location = new System.Drawing.Point(12, 52);
+            this.tv_drops.Name = "tv_drops";
+            this.tv_drops.Size = new System.Drawing.Size(260, 236);
+            this.tv_drops.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "If an entry is red, only one of the items will be dropped";
+            // 
+            // cb_expandTree
+            // 
+            this.cb_expandTree.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb_expandTree.AutoSize = true;
+            this.cb_expandTree.Location = new System.Drawing.Point(210, 14);
+            this.cb_expandTree.Name = "cb_expandTree";
+            this.cb_expandTree.Size = new System.Drawing.Size(62, 17);
+            this.cb_expandTree.TabIndex = 3;
+            this.cb_expandTree.Text = "Expand";
+            this.cb_expandTree.UseVisualStyleBackColor = true;
+            this.cb_expandTree.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // DropOverview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 300);
+            this.Controls.Add(this.cb_expandTree);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tv_drops);
+            this.Controls.Add(this.cb_dropTables);
+            this.MinimumSize = new System.Drawing.Size(300, 100);
+            this.Name = "DropOverview";
+            this.ShowIcon = false;
+            this.Text = "DropOverview";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_dropTables;
+        private System.Windows.Forms.TreeView tv_drops;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cb_expandTree;
+    }
+}
