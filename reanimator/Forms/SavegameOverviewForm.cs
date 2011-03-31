33e43ec7f2422cfc0f98174fa10d6e57839ae67b
KiteDX <KiteDX@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-03-31 14:28:56 +0000

    Removed an obsolete code file from the project Some more savegame infos

--- reanimator/Forms/SavegameOverviewForm.cs
+++ reanimator/Forms/SavegameOverviewForm.cs
@@ -26,33 +26,57 @@ namespace Reanimator.Forms
 
             treeViewSkills.Nodes.Clear();
             CharacterSkills skills = unit.CharacterWrapper.CharacterSkills;
+            CharacterValues values = unit.CharacterWrapper.CharacterValues;
 
+            AddCharacterStats(values);
+
+            AddGeneralSkills(skills);
+
+            AddCharacterSkills(skills);
+        }
+
+        private void AddCharacterStats(CharacterValues values)
+        {
+            TreeNode characterValues = new TreeNode("Character stats");
+
+            TreeNode accuracy = new TreeNode("Accuracy: " + values.Accuracy);
+            accuracy.ForeColor = Color.Red;
+            TreeNode strength = new TreeNode("Strength: " + values.Strength);
+            strength.ForeColor = Color.Red;
+            TreeNode stamina = new TreeNode("Stamina: " + values.Stamina);
+            stamina.ForeColor = Color.Red;
+            TreeNode willpower = new TreeNode("Willpower: " + values.Willpower);
+            willpower.ForeColor = Color.Red;
+            TreeNode availableAttributePoints = new TreeNode("Available attribute points: " + values.AttributePoints);
+            availableAttributePoints.ForeColor = Color.Red;
+            TreeNode availableSkillPoints = new TreeNode("Available skill points: " + values.SkillPoints);
+            availableSkillPoints.ForeColor = Color.Red;
+            TreeNode experiencePoints = new TreeNode("Experience points: " + values.Experience);
+            experiencePoints.ForeColor = Color.Red;
+            TreeNode achievementPoints = new TreeNode("Achievement points: " + values.AchievementPointsCur);
+            achievementPoints.ForeColor = Color.Red;
+
+            characterValues.Nodes.Add(accuracy);
+            characterValues.Nodes.Add(strength);
+            characterValues.Nodes.Add(stamina);
+            characterValues.Nodes.Add(willpower);
+            characterValues.Nodes.Add(availableAttributePoints);
+            characterValues.Nodes.Add(availableSkillPoints);
+            characterValues.Nodes.Add(experiencePoints);
+            characterValues.Nodes.Add(achievementPoints);
+
+            treeViewSkills.Nodes.Add(characterValues);
+        }
+
+        private void AddCharacterSkills(CharacterSkills skills)
+        {
             int counter = 0;
-
-            TreeNode general = new TreeNode("General skills (bugged for Ressurection -> where do all those extra skills come from?)");
-
-            foreach (Skill skill in skills.GeneralSkills.Skills)
-            {
-                TreeNode subNode = new TreeNode(skill.Name);
-                subNode.ForeColor = Color.Red;
-                subNode.Nodes.Add("ID: " + skill.SkillID);
-                subNode.Nodes.Add("Learned: " + skill.Learned);
-                subNode.Nodes.Add("Level: " + skill.CurrentLevel + "/" + skill.MaxLevel);
-                subNode.Nodes.Add("Description: " + skill.Description);
-                subNode.Nodes.Add("Position: " + skill.Position.X + "," + skill.Position.Y);
-                subNode.Nodes.Add("Icon name: " + skill.IconName);
-                subNode.Nodes.Add("Required skill (ID): " + (skill.RequiredSkills.Length > 0 ? skill.RequiredSkills[0].ToString() : "none"));
-                subNode.Nodes.Add("Required skill (level): " + (skill.LevelsOfRequiredSkills.Length > 0 ? skill.LevelsOfRequiredSkills[0].ToString() : "none"));
-                general.Nodes.Add(subNode);
-            }
-
-            treeViewSkills.Nodes.Add(general);
 
             foreach (SkillTab skillTab in skills.SkillTabs)
             {
                 counter++;
                 string name = "Perks (should not be displayed in Singleplayer)";
-                switch(counter)
+                switch (counter)
                 {
                     case 1:
                         name = "Skill Page 1";
@@ -63,7 +87,7 @@ namespace Reanimator.Forms
                     case 3:
                         name = "Skill Page 3";
                         break;
-                }                
+                }
 
                 TreeNode node = new TreeNode(name);
 
@@ -85,6 +109,28 @@ namespace Reanimator.Forms
 
                 treeViewSkills.Nodes.Add(node);
             }
+        }
+
+        private void AddGeneralSkills(CharacterSkills skills)
+        {
+            TreeNode general = new TreeNode("General skills (bugged for Ressurection -> where do all those extra skills come from?)");
+
+            foreach (Skill skill in skills.GeneralSkills.Skills)
+            {
+                TreeNode subNode = new TreeNode(skill.Name);
+                subNode.ForeColor = Color.Red;
+                subNode.Nodes.Add("ID: " + skill.SkillID);
+                subNode.Nodes.Add("Learned: " + skill.Learned);
+                subNode.Nodes.Add("Level: " + skill.CurrentLevel + "/" + skill.MaxLevel);
+                subNode.Nodes.Add("Description: " + skill.Description);
+                subNode.Nodes.Add("Position: " + skill.Position.X + "," + skill.Position.Y);
+                subNode.Nodes.Add("Icon name: " + skill.IconName);
+                subNode.Nodes.Add("Required skill (ID): " + (skill.RequiredSkills.Length > 0 ? skill.RequiredSkills[0].ToString() : "none"));
+                subNode.Nodes.Add("Required skill (level): " + (skill.LevelsOfRequiredSkills.Length > 0 ? skill.LevelsOfRequiredSkills[0].ToString() : "none"));
+                general.Nodes.Add(subNode);
+            }
+
+            treeViewSkills.Nodes.Add(general);
         }
 
         public void AddCharacter(UnitWrapper2 wrapper)
