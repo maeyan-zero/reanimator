c9a68482e3369f8afd990311ef048ca3528c14f2
alexander.muhle@gmail.com <alexander.muhle@gmail.com@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-04-01 23:19:38 +0000

    fixed TextureSheetPreview to properly resize

--- trunk/reanimator/Forms/TextureSheetPreview.Designer.cs
+++ trunk/reanimator/Forms/TextureSheetPreview.Designer.cs
@@ -66,6 +66,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(12, 41);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(319, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
