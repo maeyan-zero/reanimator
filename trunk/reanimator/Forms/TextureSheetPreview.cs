1ab76537dbccb20cb284287ec7a7b730b0a55a87
KiteDX <KiteDX@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-04-01 22:58:09 +0000

    Combined texture previews

--- trunk/reanimator/Forms/TextureSheetPreview.cs
+++ trunk/reanimator/Forms/TextureSheetPreview.cs
@@ -7,12 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Reanimator.Forms.HeroEditorFunctions;
+using System.IO;
 
 namespace Reanimator.Forms
 {
     public partial class TextureSheetPreview : Form
     {
         AtlasImageLoader loader;
+        Bitmap bmp;
+        string name;
 
         public TextureSheetPreview()
         {
@@ -26,11 +29,28 @@ namespace Reanimator.Forms
             comboBox1.DataSource = loader.GetImageNames();
         }
 
+        public void SetBitmap(Bitmap bmp, string name)
+        {
+            this.bmp = bmp;
+            this.name = name;
+            comboBox1.Enabled = false;
+
+            pictureBox1.Image = bmp;
+        }
+
         private void buttonSave_Click(object sender, EventArgs e)
         {
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                loader.SaveImagesToFolder(folderBrowserDialog1.SelectedPath);
+                if (loader != null)
+                {
+                    loader.SaveImagesToFolder(folderBrowserDialog1.SelectedPath);
+                }
+                else
+                {
+                    string path = Path.Combine(folderBrowserDialog1.SelectedPath, name + ".bmp");
+                    bmp.Save(path);
+                }
             }
         }
 
