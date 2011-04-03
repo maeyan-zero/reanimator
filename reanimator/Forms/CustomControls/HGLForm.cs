c6ca3e2231f7eb8353c10d43ba46533328967a63
KiteDX <KiteDX@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-04-03 11:55:51 +0000

    Added missing files

---
+++ reanimator/Forms/CustomControls/HGLForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Hellgate;
+
+namespace Reanimator.Forms.CustomControls
+{
+    public partial class HGLForm : Form
+    {
+        FileManager fileManager;
+        
+
+        public HGLForm(FileManager fileManager)
+        {
+            InitializeComponent();
+
+            this.fileManager = fileManager;
+        }
+    }
+}
