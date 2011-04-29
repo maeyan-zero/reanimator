6ec310bcb72c230ac0c74213b836fbb0e41c35c4
jhyman76 <jhyman76@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-04-29 06:23:57 +0000

    Fixes for March Resurrection client

---
+++ trunk/hellgate/Excel/JapaneseBeta/Action.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Hellgate.Excel.JapaneseBeta
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    class ActionBeta
+    {
+        ExcelFile.RowHeader header;
+        [ExcelFile.OutputAttribute(SortColumnOrder = 1)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+        public String name;
+        public Int32 unknown1;
+        public Int32 unknown2;
+        public Int32 unknown3;
+        public Int32 unknown4;
+        public Int32 unknown5;
+        public Int32 unknown6;
+        public Int32 unknown7;
+    }
+}
