48a4407ee6feb06326ee1f45e15a7cf4134150b2
alex.waters@gmail.com <alex.waters@gmail.com@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-07-06 11:16:45 +0000

    Completed definition of Action excel table (Resurrection). Updated ExcelFile reading method to have same flow as Client reads (can now read "empty" files etc).

--- trunk/hellgate/Excel/JapaneseBeta/Action.cs
+++ trunk/hellgate/Excel/JapaneseBeta/Action.cs
@@ -10,12 +10,19 @@ namespace Hellgate.Excel.JapaneseBeta
         [ExcelFile.OutputAttribute(SortColumnOrder = 1)]
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
         public String name;
-        public Int32 unknown1;
-        public Int32 unknown2;
-        public Int32 unknown3;
-        public Int32 unknown4;
-        public Int32 unknown5;
-        public Int32 unknown6;
-        public Int32 unknown7;
+        [ExcelFile.Output(IsTableIndex = true, TableStringId = "SKILLS")] // 0x29
+        public Int32 startSkill;
+        [ExcelFile.Output(IsTableIndex = true, TableStringId = "STATES")] // 0x4B
+        public Int32 stateSuccess;
+        [ExcelFile.Output(IsTableIndex = true, TableStringId = "STATES")] // 0x4B
+        public Int32 stateFail;
+        [ExcelFile.Output(IsTableIndex = true, TableStringId = "STATES")] // 0x4B
+        public Int32 stateDestroy;
+        [ExcelFile.Output(IsStringIndex = true)]
+        public Int32 stringSuccess;
+        [ExcelFile.Output(IsStringIndex = true)]
+        public Int32 stringFail;
+        [ExcelFile.Output(IsStringIndex = true)]
+        public Int32 stringDestroy;
     }
 }
