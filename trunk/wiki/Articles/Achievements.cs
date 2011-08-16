b2fe7a5da5f36305b68ec9b35976e9bd11083fda
firvagor@gmail.com <firvagor@gmail.com@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-08-16 04:31:43 +0000

    added image wiki syntax generator; added function to return table schema and insert statements as a single string; added optional (for now) parameter to WikiScript constructor that supplies a table name; added class to partially automate table scripting, and a function that aims to replace the other two SQL exports; added achievements SQL export

--- trunk/wiki/Articles/Achievements.cs
+++ trunk/wiki/Articles/Achievements.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Data;
-using System.Diagnostics;
 using System.Text;
 using Hellgate;
 
@@ -8,7 +7,7 @@ namespace MediaWiki.Articles
 {
     class Achievements : WikiScript
     {
-        public Achievements(FileManager manager) : base(manager)
+        public Achievements(FileManager manager) : base(manager,"achievements")
         {
         }
 
@@ -38,7 +37,7 @@ namespace MediaWiki.Articles
                     var skill = skills.Rows[s]["effectString_string"].ToString();
                     var var = skills.Rows[s]["skillVar1"].ToString();
                     var icon = skills.Rows[s]["largeIcon"].ToString();
-                    icon = "[[File:" + icon + ".png|40px]] ";
+                    icon = GetImage(icon + ".png", 40);// "[[File:" + icon + ".png|40px]] ";
                     var = var.Replace(";", "");
                     skill = skill.Replace("[string2]", var);
                     builder.AppendLine("|" + icon + skill);
@@ -56,14 +55,124 @@ namespace MediaWiki.Articles
             return builder.ToString();
         }
 
+        public override string ExportTableInsertScript()
+        {
+            TableScript = new SQLTableScript("id",
+                "id INT NOT NULL",
+                "name TEXT NOT NULL",
+                "description TEXT NOT NULL",
+                "skill_file TEXT",
+                "skill_text TEXT",
+                "title TEXT"
+                );
+
+            var achievements = Manager.GetDataTable("ACHIEVEMENTS");
+            var skills = Manager.GetDataTable("SKILLS");
+
+            object id;
+            string name, desc, skillFile, skillText, title;
+            foreach (DataRow row in achievements.Rows)
+            {
+                id = row["code"];
+                name = "\"" + row["nameString_string"] + "\"";
+
+                desc = row["descripFormatString_string"].ToString();
+                desc = desc.Replace("[completenum]", String.Format("{0:#,0}", row["completeNumber"]));
+                desc = desc.Replace("[param1]", String.Format("{0:#,0}", (int)row["param1"] / 20));
+                desc = "\"" + desc + "\"";
+
+                if ((int)row["rewardSkill"] != -1)
+                {
+                    var s = (int)row["rewardSkill"];
+                    var skill = skills.Rows[s]["effectString_string"].ToString();
+                    var var = skills.Rows[s]["skillVar1"].ToString();
+                    var = var.Replace(";", "");
+                    skill = skill.Replace("[string2]", var);
+
+                    var icon = skills.Rows[s]["largeIcon"].ToString();
+
+                    skillFile =  "\""+icon + ".png\"";
+                    skillText = "\"" + skill + "\"";
+                }
+                else
+                {
+                    skillFile = skillText = "''";
+                }
+
+                title = row["rewardTitle_string"].ToString();
+                title = title.Replace("[PLAYERNAME]", "").Trim();
+                title = "\"" + title + "\"";
+
+                TableScript.AddRow(id.ToString(), name, desc, skillFile, skillText, title);
+            }
+
+            return TableScript.GetFullScript();
+        }
+
         public override string ExportSchema()
         {
-            throw new NotImplementedException();
+            var schema = "DROP TABLE IF EXISTS " + FullTableName + ";\n" +
+            "CREATE TABLE " + FullTableName + @" (
+             id INT NOT NULL,
+             name TEXT NOT NULL,
+             description TEXT NOT NULL,
+             skill_file TEXT,
+             skill_text TEXT,
+             title TEXT,
+             PRIMARY KEY(id)
+             );";
+            return schema;
         }
 
         public override string ExportTable()
         {
-            throw new NotImplementedException();
+            var achievements = Manager.GetDataTable("ACHIEVEMENTS");
+            var skills = Manager.GetDataTable("SKILLS");
+
+            var builder = new StringBuilder();
+            builder.AppendLine("INSERT INTO " + FullTableName);
+            builder.AppendLine("VALUES");
+
+            foreach (DataRow row in achievements.Rows)
+            {
+                builder.Append("(");
+                builder.Append(row["code"] + ", ");
+                builder.Append("\""+row["nameString_string"] + "\", ");
+
+                var descrip = row["descripFormatString_string"].ToString();
+                descrip = descrip.Replace("[completenum]", String.Format("{0:#,0}", row["completeNumber"]));
+                descrip = descrip.Replace("[param1]", String.Format("{0:#,0}", (int)row["param1"] / 20));
+                builder.Append("\"" + descrip + "\", ");
+
+                if ((int)row["rewardSkill"] != -1)
+                {
+                    var s = (int)row["rewardSkill"];
+                    var skill = skills.Rows[s]["effectString_string"].ToString();
+                    var var = skills.Rows[s]["skillVar1"].ToString();
+                    var = var.Replace(";", "");
+                    skill = skill.Replace("[string2]", var);
+
+                    var icon = skills.Rows[s]["largeIcon"].ToString();
+                    icon += ".png";
+
+                    builder.Append("\"" + icon + "\", ");
+                    builder.Append("\"" + skill + "\", ");
+                }
+                else
+                {
+                    builder.Append("'', ");
+                    builder.Append("'', ");
+                }
+
+                var title = row["rewardTitle_string"].ToString();
+                title = title.Replace("[PLAYERNAME]", "").Trim();
+
+                builder.Append("\"" + title + "\"");
+                builder.AppendLine("),");
+            }
+            builder.Remove(builder.Length - 3, 3);
+            builder.AppendLine(";");
+            return builder.ToString();
         }
     }
 }
