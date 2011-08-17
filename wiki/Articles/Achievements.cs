44701bee63079165580d7b4ad4ff05d59ee8b350
firvagor@gmail.com <firvagor@gmail.com@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-08-17 03:37:09 +0000

    corrected image wiki syntax; added index argument for generating SQL table script; added validation for empty value strings when inserting table rows; created class to partially automate wiki table syntax; added wiki table export for Nano Forge upgrades; updated achievements and monsters exporting

--- wiki/Articles/Achievements.cs
+++ wiki/Articles/Achievements.cs
@@ -17,47 +17,50 @@ namespace MediaWiki.Articles
             var achievements = Manager.GetDataTable("ACHIEVEMENTS");
             var skills = Manager.GetDataTable("SKILLS");
 
-            builder.AppendLine("{| class=\"wikitable\"");
-            builder.AppendLine("!Name");
-            builder.AppendLine("!Description");
-            builder.AppendLine("!Reward Skill");
-            builder.AppendLine("!Reward Title");
+            WikiTable table = new WikiTable(false, string.Empty, string.Empty,
+                "Name",
+                "Description",
+                "Reward Skill",
+                "Reward Title"
+                );
 
+            string name, desc, skill, title;
             foreach (DataRow row in achievements.Rows)
             {
-                builder.AppendLine("|-");
-                builder.AppendLine("|" + row["nameString_string"]);
-                var descrip = row["descripFormatString_string"].ToString();
-                descrip = descrip.Replace("[completenum]", String.Format("{0:#,0}", row["completeNumber"]));
-                descrip = descrip.Replace("[param1]", String.Format("{0:#,0}", (int) row["param1"] / 20));
-                builder.AppendLine("|" + descrip);
-                if ((int) row["rewardSkill"] != -1)
+                name = row["nameString_string"].ToString();
+
+                desc = row["descripFormatString_string"].ToString();
+                desc = desc.Replace("[completenum]", String.Format("{0:#,0}", row["completeNumber"]));
+                desc = desc.Replace("[param1]", String.Format("{0:#,0}", (int)row["param1"] / 20));
+
+                if ((int)row["rewardSkill"] != -1)
                 {
-                    var s = (int) row["rewardSkill"];
-                    var skill = skills.Rows[s]["effectString_string"].ToString();
+                    var s = (int)row["rewardSkill"];
+                    skill = skills.Rows[s]["effectString_string"].ToString();
                     var var = skills.Rows[s]["skillVar1"].ToString();
                     var icon = skills.Rows[s]["largeIcon"].ToString();
                     icon = GetImage(icon + ".png", 40);// "[[File:" + icon + ".png|40px]] ";
                     var = var.Replace(";", "");
                     skill = skill.Replace("[string2]", var);
-                    builder.AppendLine("|" + icon + skill);
+                    skill = icon + " " + skill;
                 }
                 else
                 {
-                    builder.AppendLine("|");
+                    skill = string.Empty;
                 }
-                var title = row["rewardTitle_string"].ToString();
+
+                title = row["rewardTitle_string"].ToString();
                 title = title.Replace("[PLAYERNAME]", "");
-                builder.AppendLine("|" + title);
+
+                table.AddRow(name, desc, skill, title);
             }
 
-            builder.AppendLine("|}");
-            return builder.ToString();
+            return table.GetTableSyntax();
         }
 
         public override string ExportTableInsertScript()
         {
-            TableScript = new SQLTableScript("id",
+            TableScript = new SQLTableScript("id", string.Empty,
                 "id INT NOT NULL",
                 "name TEXT NOT NULL",
                 "description TEXT NOT NULL",
@@ -96,7 +99,7 @@ namespace MediaWiki.Articles
                 }
                 else
                 {
-                    skillFile = skillText = "''";
+                    skillFile = skillText = string.Empty;
                 }
 
                 title = row["rewardTitle_string"].ToString();
@@ -109,6 +112,7 @@ namespace MediaWiki.Articles
             return TableScript.GetFullScript();
         }
 
+        [Obsolete("This class is now using ExportTableInsertScript instead")]
         public override string ExportSchema()
         {
             var schema = "DROP TABLE IF EXISTS " + FullTableName + ";\n" +
@@ -123,7 +127,7 @@ namespace MediaWiki.Articles
              );";
             return schema;
         }
-
+        [Obsolete("This class is now using ExportTableInsertScript instead")]
         public override string ExportTable()
         {
             var achievements = Manager.GetDataTable("ACHIEVEMENTS");
--- wiki/Articles/Monsters.cs
+++ wiki/Articles/Monsters.cs
@@ -9,15 +9,93 @@ namespace MediaWiki.Articles
 {
     class Monsters : WikiScript
     {
-        public Monsters(FileManager manager) : base(manager)
+        public Monsters(FileManager manager) : base(manager,"monsters")
         {
         }
 
         public override string ExportArticle()
         {
+            //not sure what we're doing here since we don't want everything at once
             throw new NotImplementedException();
         }
 
+        public override string ExportTableInsertScript()
+        {
+            SQLTableScript table = new SQLTableScript("id", "code",
+                "id INT NOT NULL",
+                "code VARCHAR(4) NOT NULL",
+                "name TEXT",
+                "base TEXT",
+                "quality TEXT",
+                "hp_min INT",
+                "hp_max INT",
+                "experience INT",
+                "armor INT",
+                "shields INT",
+                "phys_atk INT",
+                "phys_def INT",
+                "fire_atk INT",
+                "fire_def INT",
+                "elec_atk INT",
+                "elec_def INT",
+                "spec_atk INT",
+                "spec_def INT",
+                "pois_atk INT",
+                "pois_def INT",
+                "treasure INT",
+                "treasure_champion INT",
+                "treasure_first INT"
+                );
+
+            var monsters = Manager.GetDataTable("MONSTERS");
+
+            string id, code, name, baseType, quality, hpMin, hpMax, xp, armor, shields, 
+                physAtk, physDef, fireAtk, fireDef, elecAtk, elecDef, specAtk, specDef, poisAtk, poisDef, 
+                treasure, treasureChamp, treasureFirst;
+
+            foreach (DataRow row in monsters.Rows)
+            {
+                id = row["Index"].ToString();
+                code = GetSqlEncapsulatedString(((int)row["code"]).ToString("X"));
+                name = GetSqlEncapsulatedString(row["String_string"] as string ?? string.Empty);
+
+                baseType = ((int)row["baseRow"] != -1 ? monsters.Rows[(int)row["baseRow"]]["String_string"] as string : "") ?? string.Empty;
+                baseType = GetWikiArticleLink(baseType);
+                baseType = GetSqlEncapsulatedString(baseType);
+
+                quality = (int)row["monsterQuality"] != -1 ? (string)row["monsterQuality_string"] : string.Empty;
+                quality = GetWikiArticleLink(quality);
+                quality = GetSqlEncapsulatedString(quality);
+
+                hpMin = row["hpMin"].ToString();
+                hpMax = row["hpMax"].ToString();
+                xp = row["experience"].ToString();
+                armor = row["armor"].ToString();
+                shields = row["shields"].ToString();
+
+                physAtk = row["sfxPhysicalAbilityPct"].ToString();
+                physDef = row["sfxPhysicalDefensePct"].ToString();
+                fireAtk = row["sfxFireAbilityPct"].ToString();
+                fireDef = row["sfxFireDefensePct"].ToString();
+                elecAtk = row["sfxElectricAbilityPct"].ToString();
+                elecDef = row["sfxElectricDefensePct"].ToString();
+                specAtk = row["sfxSpectralAbilityPct"].ToString();
+                specDef = row["sfxSpectralDefensePct"].ToString();
+                poisAtk = row["sfxToxicAbilityPct"].ToString();
+                poisDef = row["sfxToxicDefensePct"].ToString();
+
+                treasure = row["treasure"].ToString();
+                treasureChamp = row["championTreasure"].ToString();
+                treasureFirst = row["firstTimeTreasure"].ToString();
+
+                table.AddRow(id, code, name, baseType, quality, hpMin, hpMax, xp, armor, shields,
+                    physAtk, physDef, fireAtk, fireDef, elecAtk, elecDef, specAtk, specDef, poisAtk, poisDef,
+                    treasure, treasureChamp, treasureFirst);
+            }
+
+            return table.GetFullScript();
+        }
+        [Obsolete("This class is now using ExportTableInsertScript instead")]
         public override string ExportSchema()
         {
             var builder = new StringBuilder();
@@ -75,7 +153,7 @@ namespace MediaWiki.Articles
             builder.AppendLine(");");
             return builder.ToString();
         }
-
+        [Obsolete("This class is now using ExportTableInsertScript instead")]
         public override string ExportTable()
         {
             var monsters = Manager.GetDataTable("MONSTERS");
