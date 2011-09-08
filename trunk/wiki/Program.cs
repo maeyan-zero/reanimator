70bc916451e50e3cf42b67abf8e8d445f882ca1a
firvagor@gmail.com <firvagor@gmail.com@0aafadf6-0629-11df-8138-214a9d281a8c>
2011-09-08 03:43:05 +0000

    corrected clvl requirement based on ilvl

--- trunk/wiki/Program.cs
+++ trunk/wiki/Program.cs
@@ -17,7 +17,7 @@ namespace MediaWiki
             manager.LoadTableFiles();
             manager.EndAllDatAccess();
 
-            //TODO: beam damage (listed as direct but apparently uses some other calculation)
+            //TODO: beam damage types (listed as direct but apparently uses some other calculation)
 
             // *****while this message is still here, correct the following manually after every item export*****
             //TODO: fix stats that are super inaccurate (manually changed in DB for now):
