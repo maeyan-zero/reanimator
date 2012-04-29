64da09be8749e90f5d68e579a2cff13bf08c24bf
alex.waters@gmail.com <alex.waters@gmail.com@0aafadf6-0629-11df-8138-214a9d281a8c>
2012-04-29 02:52:19 +0000

    Enable serialization methods on SP Xls definitions.

--- trunk/hellgate/Excel/SinglePlayer/ColorSets.cs
+++ trunk/hellgate/Excel/SinglePlayer/ColorSets.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace Hellgate.Excel.SinglePlayer
 {
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    [Serializable, StructLayout(LayoutKind.Sequential, Pack = 1)]
     public class ColorSets
     {
         ExcelFile.RowHeader header;
